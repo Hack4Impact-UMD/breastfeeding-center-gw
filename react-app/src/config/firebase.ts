b2ac0e12567feb1e7dacc82954d667860e804286
pg1211 <143668413+pg1211@users.noreply.github.com>
2025-03-10 17:42:11 -0400

    testing stuff

--- react-app/src/config/firebase.ts
+++ react-app/src/config/firebase.ts
@@ -3,7 +3,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // import { getAnalytics } from "firebase/analytics";
-import { getFunctions /*connectFunctionsEmulator*/ } from "firebase/functions";
 import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -30,6 +29,5 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const functions = getFunctions(app, "us-east4");
-// connectFunctionsEmulator(functions, "127.0.0.1", 5001);
 connectFunctionsEmulator(functions, "127.0.0.1", 5001);
 export default app;
