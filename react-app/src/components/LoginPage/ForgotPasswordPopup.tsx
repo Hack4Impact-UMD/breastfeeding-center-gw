3c54ebcef0b5768324b327643830d8c9be29cea3
patrickhong <path.plh@gmail.com>
2025-03-17 20:17:35 -0400

    forgot password popup

---
+++ react-app/src/components/LoginPage/ForgotPasswordPopup.tsx
@@ -0,0 +1,70 @@
+/*
+- Create a Forgot Password Popup (no reset password functionality)
+- Refer to Figma in High Fidelity file
+
+- If the Forgot password button is clicked on the login page, the Forgot Your Password popup will open
+- Use the ModalWrapper in the Components folder as a base for the popup
+    - Feel free to modify ModalWrapper as needed to fit styles
+- Create new LoginPage folder and put these popup(s) and the LoginPage in this file
+- The first popup should display the following:
+    - Title & subtext
+    - Input for Email
+    - “Submit button
+    - ← on the top left corner that will close out the popup
+- The second popup:
+    - Simply display the success title and subtext
+    - `X` will close out of the popup
+- To implement this popup, you can either:
+    1. Simply have one popup that toggles between the information (once submit is clicked, display success information on same popup). This is the preferred method
+    2. Create 2 separate popups for this
+*/
+
+import Modal from "../Modal";
+import { BiArrowBack } from "react-icons/bi";
+
+const ForgotPassword = (): React.JSX.Element => {
+  const content = (
+    <div>
+      {/* Cursor becomes pointer over the padding */}
+      <button
+        onClick={() => console.log("Should close out popup")}
+        className="cursor-pointer p-4"
+      >
+        <BiArrowBack className="w-8 h-6"></BiArrowBack>
+      </button>
+      <div className="flex flex-col justify-center px-15">
+        {/* Need to import Monsterrat into tailwind and need to center these elements properly */}
+        <h1 className="text-[1.7rem] font-semibold font-Monsterrat">
+          Forgot Your Password?
+        </h1>
+        <p className="leading-tight my-4 font-inter">
+          Please enter your email address below. You will receive a link to
+          create a new password.{" "}
+        </p>
+        <p className="font-Inter">Email Address</p>
+        <input className="h-15 border-1 border-black" type="text" required />
+        <button
+          onClick={() =>
+            console.log("Should submit address and next popup appears")
+          }
+          className="my-4 w-3/4 h-17 cursor-pointer rounded-full bg-[#f4bb47] self-center text-[1.6rem] font-bold font-inter"
+        >
+          {/* Don't know what the font is used for submit */}
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+  return (
+    <div>
+      <Modal
+        open={true}
+        onClose={() => console.log("Go back to login page")}
+        children={content}
+        height={400}
+      />
+    </div>
+  );
+};
+
+export default ForgotPassword;
