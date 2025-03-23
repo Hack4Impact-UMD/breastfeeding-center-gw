c4415fc7ff761319cb336ec7a76946762d81e123
Sophie Tsai <stsai123@umd.edu>
2025-03-23 02:08:40 -0400

    Add Montserrat and new bcgw colors, clean not found page styles

--- react-app/src/pages/NotFoundPage.tsx
+++ react-app/src/pages/NotFoundPage.tsx
@@ -4,23 +4,17 @@ import primaryLogo from "../assets/bcgw-logo.png";
 function NotFoundPage() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center h-screen text-center space-y-2 p-2">
+      <div className="flex flex-col items-center justify-center h-screen text-center space-y-2">
         <img
           className="h-50 w-50 object-contain"
           src={primaryLogo}
           alt="BCGW Logo"
         />
-        <p className="text-8xl font-extrabold font-[Roboto] tracking-[8px]">
-          404
-        </p>
-        <h2 className="font-[Roboto] font-semibold tracking-[2px]">
-          Oops! Page Not Found
-        </h2>
-        <h3 className="font-Inter">
-          The page you are looking for is unavailable or missing.
-        </h3>
+        <p className="text-8xl font-extrabold tracking-[8px]">404</p>
+        <h2 className="font-semibold tracking-[2px]">Oops! Page Not Found</h2>
+        <h3>The page you are looking for is unavailable or missing.</h3>
         <Link to="/">
-          <p className="font-Inter underline text-[#3a3a3a]">RETURN TO HOME</p>
+          <p className="underline mt-2">RETURN TO HOME</p>
         </Link>
       </div>
     </>
