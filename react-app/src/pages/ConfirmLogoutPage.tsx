95331b12f15f6fc73252b6991698b27463cb44a8
Emily Li <emilyli5@umd.edu>
2025-03-24 18:50:44 -0400

    better css

--- react-app/src/pages/ConfirmLogoutPage.tsx
+++ react-app/src/pages/ConfirmLogoutPage.tsx
@@ -17,21 +17,25 @@ export default function ConfirmLogoutPage() {
         </button>
   
         <Modal open={isOpen} onClose={() => setIsOpen(false)} height={300}>
-          <div className="relative p-8 flex flex-col items-center justify-center text-center w-[450px] h-[330px]">
-            <button
-              onClick={() => setIsOpen(false)}
-              className="absolute top-4 right-4 text-gray-600"
-            >
-              <IoCloseCircleOutline size={50} />
-            </button>
-            <h2 className="text-xl font-semibold flex-grow flex items-center justify-center">Are you sure you want to log out?</h2>
-            <div className="flex justify-center gap-6 w-full pb-8">
-              <button onClick={() => setIsOpen(false)} className="w-1/3 rounded-lg px-5 py-3 border border-black">
+        <button
+            onClick={() => setIsOpen(false)}
+            className="absolute top-4 right-4 text-gray-600 z-10"
+        >
+            <IoCloseCircleOutline size={50} />
+        </button>
+        <div className="relative p-8 flex flex-col items-center justify-center text-center h-full">
+            
+            <h2 className="text-xl font-semibold flex-grow flex items-center justify-center mt-[20px] mb-[0px]">Are you sure you want to log out?</h2>
+            <div className="flex justify-center gap-6 pt-4 pb-8">
+              <button 
+                onClick={() => setIsOpen(false)} 
+                className="flex-1 rounded-lg px-5 py-3 border border-black"
+              >
                 Cancel
               </button>
               <button
                 onClick={() => navigate("/logout")}
-                className="w-1/3 bg-bcgw-yellow-dark rounded-lg px-5 py-3 border border-black"
+                className="flex-1 bg-bcgw-yellow-dark rounded-lg px-5 py-3 border border-black"
               >
                 Confirm
               </button>
