f4e5e07a0e6a1d093de0aaefe6bc3800870b8e13
Margaret Kato <minim4455@gmail.com>
2025-03-30 19:51:44 -0400

    extracted all columns from excel sheet, added visit type key based on treatmentt_name column

--- react-app/src/pages/Test.tsx
+++ react-app/src/pages/Test.tsx
@@ -1,32 +1,86 @@
 import React from "react";
 import * as XLSX from "xlsx";
+
+interface Appointment {
+  id: string;
+  location_name: string;
+  start_at: string;
+  end_at: string;
+  patient_guid: string;
+  patient_number: string;
+  patient_prefix: string;
+  patient_first_name: string;
+  patient_preferred_name: string;
+  patient_last_name: string;
+  treatment_name: string;
+  staff_member_name: string;
+  break: string;
+  insurance_state: string;
+  state: string;
+  first_visit: string;
+  chart_status: string;
+  notes_text: string;
+  visit_type: string; 
+}
+
 function Test() {
   const handleFile = async (
     e: React.ChangeEvent<HTMLInputElement>
-  ): Promise<Array<{ parent: any; path: any }>> => {
+  ): Promise<void> => {
     const file = e.target.files![0];
     const data = await file.arrayBuffer();
     const workbook = XLSX.read(data);
     const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-    //const jsonData = XLSX.utils.sheet_to_json(worksheet);
+
+    // convert excel sheet to JSON
     const jsonData = XLSX.utils.sheet_to_json(worksheet, {
-      header: ["parent", "path"],
-      range: 1,
-    }) as Array<{ parent: any; path: any }>;
+      header: 1, 
+    });
 
-    const parentValues = jsonData.map((row) => row.parent);
+    const headers: string[] = jsonData[0] as string[];
 
-    const pathValues = jsonData.map((row) => row.path);
+    // these are target columns in excel sheet
+    const relevantColumns = [
+      "id", "location_name", "start_at", "end_at", "patient_guid", 
+      "patient_number", "patient_prefix", "patient_first_name", 
+      "patient_preferred_name", "patient_last_name", "treatment_name", 
+      "staff_member_name", "break", "insurance_state", "state", 
+      "first_visit", "chart_status", "notes_text"
+    ];
 
-    console.log("Parent column values:", parentValues);
-    console.log("Path column values:", pathValues);
+    // get idx of target columns 
+    const columnIndexes = relevantColumns.map((col) => headers.indexOf(col));
 
-    return jsonData;
+    // get info from each col for each appointment
+    const extractedAppointments: Appointment[] = (jsonData.slice(1) as any[]).map((row: string[]) => {
+      const appointment = {} as Appointment;
+
+      relevantColumns.forEach((column, index) => {
+        appointment[column as keyof Appointment] = row[columnIndexes[index]] || "";
+      });
+
+      // get visit type from treatment_name
+      const treatment = appointment.treatment_name.toLowerCase();
+      let visitType = ""
+
+      if (treatment.includes("telehealth")) {
+        visitType = "telehealth";
+      } else if (treatment.includes("home visit")) {
+        visitType = "home visit";
+      } else if (treatment.includes("dc office")) {
+        visitType = "dc office";
+      }
+
+      appointment.visit_type = visitType;
+
+      return appointment;
+    });
+
+    console.log(extractedAppointments);  
   };
 
   const printFile = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const data = await handleFile(e);
-    console.log(data);
+    await handleFile(e);
   };
 
   return (
