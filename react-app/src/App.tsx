21fa49530235038422c244d7232e58cff6563bda
Sophie Tsai <stsai123@umd.edu>
2025-04-03 14:57:45 -0400

    Update bcgw color variables, slightly update nav bar styles, remove unused imports

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Test from "./pages/test";
-import Test2 from "./pages/test2";
-import Test3 from "./pages/test3";
-import TestService from "./pages/testService1";
-import TestService2 from "./pages/testService2";
 
 function App() {
   return (
@@ -24,19 +19,11 @@ function App() {
           element={
             <button
               className={"bg-bcgw-yellow-dark rounded-lg px-2 py-1 m-2"}
-              onClick={async () => {}}
-            >
+              onClick={async () => {}}>
               TEST
             </button>
           }
         />
-        <Route path="/" element={<Test />} />
-        <Route path="/test2" element={<Test2 />} />
-        <Route path="/test3" element={<Test3 />} />
-        <Route path="/testService" element={<TestService />} />
-        <Route path="/testService2" element={<TestService2 />} />
-
-
       </Routes>
     </BrowserRouter>
   );
--- react-app/src/components/NavigationBar.tsx
+++ react-app/src/components/NavigationBar.tsx
@@ -11,14 +11,13 @@ import { LuChevronsRight } from "react-icons/lu";
 const NavigationBar = () => {
   // tailwind class styling into consts
   const notActiveStyle =
-    "hover:bg-[#FFEAC1] bg-white w-[100%] h-[100%] p-3 flex items-center";
-  const activeStyle = "bg-[#F5BB47] w-[100%] h-[100%] p-3 flex items-center";
-  const activeIconStyle = "bg-[#F5BB47] w-[100%] h-[100%] p-4";
-  const notActiveIconStyle = "p-2";
+    "hover:bg-bcgw-yellow-light bg-white w-full h-full p-3 flex items-center";
+  const activeStyle = "bg-bcgw-yellow-dark w-full h-full p-3 flex items-center";
+  const activeIconStyle = "bg-bcgw-yellow-dark w-full p-4";
+  const notActiveIconStyle = "p-4";
   const categoryMargin = "ml-4";
-  const serviceMargin = "ml-4"; //added this in case we want to change margin on service items but thought it looked ok with ml-4
-  const serviceStyle =
-    "flex flex-row w-[100%] h-[30px] border-t border-[#D9D9D9] cursor-pointer";
+  const serviceMargin = "ml-12";
+  const serviceStyle = "flex flex-row w-full h-[30px] cursor-pointer";
   const [openServices, setOpenServices] = useState(false);
   const [navBarOpen, setNavBarOpen] = useState(true);
 
@@ -32,23 +31,22 @@ const NavigationBar = () => {
 
   return (
     <div
-      className={`flex flex-col justify-left h-screen bg-white fixed top-[0] left-[0] shadow-[4px_4px_4px_0px_rgba(0,_0,_0,_0.25)] transition-all duration-200 ease-in-out ${
+      className={`flex flex-col justify-left h-screen fixed shadow-[4px_4px_4px_0px_rgba(0,_0,_0,_0.25)] transition-all duration-200 ease-in-out ${
         navBarOpen ? "w-[250px]" : "w-[60px]"
-      }`}
-    >
+      }`}>
       <div className="flex flex-col h-full ">
         <div className="flex flex-row justify-between items-center pr-3 pt-1">
           <button onClick={toggleNavBar} className="flex ml-auto">
             {navBarOpen ? ( //nav bar open chevron
               <LuChevronsLeft
                 className="w-8 h-8 cursor-pointer"
-                color="black"
+                color="var(--color-bcgw-blue-dark)"
               />
             ) : (
               //nav bar closed chevron
               <LuChevronsRight
                 className="w-8 h-8 cursor-pointer"
-                color="black"
+                color="var(--color-bcgw-blue-dark)"
               />
             )}
           </button>
@@ -61,175 +59,164 @@ const NavigationBar = () => {
                 src={primaryLogo}
                 alt="BCGW Logo"
               />
-              <p className="font-semibold text-2xl text-black pb-1">
+              <p className="font-semibold text-2xl text-bcgw-blue-dark pb-1">
                 Dashboard
               </p>
             </div>
 
             {/*HOME*/}
-            <div className="flex flex-col items-center mt-5 border-x border-[#D9D9D9] divide-y-1 divide-[#D9D9D9] overflow-y-auto">
-              <div className="flex flex-row border-t border-[#D9D9D9] w-[100%] cursor-pointer">
+            <div className="flex flex-col items-center mt-5 border-x border-bcgw-gray-light divide-y-1 divide-bcgw-gray-light overflow-y-auto">
+              <div className="flex flex-row border-t border-bcgw-gray-light w-full cursor-pointer">
                 <NavLink
                   to="/"
                   className={({ isActive }) =>
                     isActive ? activeStyle : notActiveStyle
-                  }
-                >
-                  <img className="w-[30px] h-[30px]" src={home} />
+                  }>
+                  <img className="w-[30px] h-[30px] " src={home} />
                   <span className={categoryMargin}>Home</span>
                 </NavLink>
               </div>
 
               {/*SERVICES*/}
-              <div className="flex flex-row w-[100%] cursor-pointer">
+              <div className="flex flex-row w-full cursor-pointer">
                 <NavLink
-                  to="/"
+                  to="/services"
                   className={({ isActive }) =>
                     isActive ? activeStyle : notActiveStyle
                   }
                   onClick={(e) => {
                     e.preventDefault();
                     toggleServicesDropdown();
-                  }}
-                >
+                  }}>
                   <div className="flex items-center justify-between w-full">
                     <div className="flex items-center">
                       <img className="w-[30px] h-[30px]" src={service} />
                       <span className={categoryMargin}>Services</span>
                     </div>
-                    {openServices ? (
-                      <span className="text-black-500">▲</span>
-                    ) : (
-                      <span className="text-black-500">▼</span>
-                    )}
-
                     {/* Dropdown triangle */}
+                    {openServices ? (
+                      <span className="text-bcgw-blue-dark">▲</span>
+                    ) : (
+                      <span className="text-bcgw-blue-dark">▼</span>
+                    )}
                   </div>
                 </NavLink>
               </div>
 
               {/*SERVICES DROPDOWN*/}
               {openServices && ( //dropdown open
-                <div className="w-full bg-white shadow-lg z-1">
+                <div className="w-full bg-white z-1">
                   <div className={`${serviceStyle} border-t-0`}>
                     <NavLink
-                      to="/"
+                      to="/services/jane"
                       className={({ isActive }) =>
                         isActive ? activeStyle : notActiveStyle
-                      }
-                    >
-                      <span className={serviceMargin}>Milk Depo</span>
+                      }>
+                      <span className={serviceMargin}>JANE</span>
                     </NavLink>
                   </div>
                   <div className={serviceStyle}>
                     <NavLink
-                      to="/"
+                      to="/services/acuity"
                       className={({ isActive }) =>
                         isActive ? activeStyle : notActiveStyle
-                      }
-                    >
-                      <span className={serviceMargin}>Group Classes</span>
+                      }>
+                      <span className={serviceMargin}>Acuity</span>
                     </NavLink>
                   </div>
                   <div className={serviceStyle}>
                     <NavLink
-                      to="/"
+                      to="/services/paysimple"
                       className={({ isActive }) =>
                         isActive ? activeStyle : notActiveStyle
-                      }
-                    >
-                      <span className={serviceMargin}>Rentals</span>
+                      }>
+                      <span className={serviceMargin}>PaySimple</span>
                     </NavLink>
                   </div>
                   <div className={serviceStyle}>
                     <NavLink
-                      to="/"
+                      to="/services/milkdepot"
                       className={({ isActive }) =>
                         isActive ? activeStyle : notActiveStyle
-                      }
-                    >
-                      <span className={serviceMargin}>Consultations</span>
+                      }>
+                      <span className={serviceMargin}>Milk Depot</span>
                     </NavLink>
                   </div>
                   <div className={serviceStyle}>
                     <NavLink
-                      to="/"
+                      to="/services/mailchimp"
                       className={({ isActive }) =>
                         isActive ? activeStyle : notActiveStyle
-                      }
-                    >
+                      }>
                       <span className={serviceMargin}>Mailchimp</span>
                     </NavLink>
                   </div>
                   <div className={serviceStyle}>
                     <NavLink
-                      to="/"
+                      to="/services/paypal"
                       className={({ isActive }) =>
                         isActive ? activeStyle : notActiveStyle
-                      }
-                    >
-                      <span className={serviceMargin}>Donations</span>
+                      }>
+                      <span className={serviceMargin}>Paypal</span>
                     </NavLink>
                   </div>
                 </div>
               )}
 
               {/*USER MANAGEMENT*/}
-              <div className="flex flex-row border-b border-[#D9D9D9] w-[100%] cursor-pointer">
+              <div className="flex flex-row border-b border-bcgw-gray-light w-full cursor-pointer">
                 <NavLink
-                  to="/"
+                  to="/management"
                   className={({ isActive }) =>
                     isActive ? activeStyle : notActiveStyle
-                  }
-                >
+                  }>
                   <img className="w-[30px] h-[30px]" src={management} />
-                  <span className={categoryMargin}>Edit Volunteers</span>
+                  <span className={categoryMargin}>User Management</span>
                 </NavLink>
               </div>
             </div>
 
             {/*LOGOUT*/}
             <div className="flex-grow"></div>
-            <div className="sticky bottom-0 bg-white pt-4 pb-4 border-t border-[#D9D9D9]">
-              <div className="flex justify-left text-[#3a3a3a] pl-[25px]">
+            <div className="sticky bottom-0 py-4 border-t border-bcgw-gray-light">
+              <button
+                className="flex justify-left items-center text-bcgw-blue-dark pl-3 gap-3 underline cursor-pointer "
+                onClick={() => {}}>
                 <img src={logout} className="w-[30px] h-[30px]" />
-                <button
-                  className="ml-4 underline cursor-pointer "
-                  onClick={() => {}}
-                >
-                  LOGOUT
-                </button>
-              </div>
+                LOGOUT
+              </button>
             </div>
           </>
         ) : (
           //nav bar closed content
-          <div className="flex flex-col items-center mb-2 h-full justify-between">
-            <div className="flex flex-col items-center space-y-5 pt-40">
+          <div className="flex flex-col items-center justify-between mb-2 h-full">
+            <div className="flex flex-col items-center justify-center space-y-5 h-full">
               <NavLink
                 to="/"
                 className={({ isActive }) =>
                   isActive ? activeIconStyle : notActiveIconStyle
-                }
-              >
+                }>
                 <img className="w-[30px] h-[30px]" src={home} />
               </NavLink>
 
-              <img
-                className="w-[30px] h-[30px] cursor-pointer"
-                src={service}
-                onClick={() => {
-                  toggleNavBar();
-                  setOpenServices(true);
-                }}
-              />
-
               <NavLink
-                to="/"
+                to="/services"
                 className={({ isActive }) =>
                   isActive ? activeIconStyle : notActiveIconStyle
                 }
-              >
+                onClick={(e) => {
+                  e.preventDefault();
+                  toggleNavBar();
+                  setOpenServices(true);
+                }}>
+                <img className="w-[30px] h-[30px]" src={service} />
+              </NavLink>
+
+              <NavLink
+                to="/management"
+                className={({ isActive }) =>
+                  isActive ? activeIconStyle : notActiveIconStyle
+                }>
                 <img className="w-[30px] h-[30px]" src={management} />
               </NavLink>
             </div>
