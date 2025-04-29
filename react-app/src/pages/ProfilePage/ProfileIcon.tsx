0fee156b1577bf651aa2cd80ba8b34a940ae6ae5
Sophie Tsai <sophietsai31@gmail.com>
2025-04-29 02:54:21 -0400

    Styling updates for profile page, move popup components into individual files, add some responsiveness

--- react-app/src/pages/ProfilePage/ProfileIcon.tsx
+++ react-app/src/pages/ProfilePage/ProfileIcon.tsx
@@ -1,7 +1,7 @@
 const ProfileIcon = ({ initials }: { initials: string }) => {
   return (
-    <div className="w-50 h-50 rounded-full bg-[#F4BB47] flex items-center justify-center text-2xl font-semibold text-black">
-      <span className="text-black font-thin text-[5rem] leading-none">
+    <div className="w-50 h-50 rounded-full bg-bcgw-yellow-dark flex items-center justify-center">
+      <span className="text-bcgw-blue-dark font-normal text-[5rem] leading-none">
         {initials}
       </span>
     </div>
