acb986b6489853c33d0898a8ed8d3f2228817a21
Ramy Kaddouri <myramkadd@gmail.com>
2025-09-24 23:15:31 -0400

    optimistic delete for better performance

--- react-app/src/hooks/mutations/useDeleteJaneRecord.ts
+++ react-app/src/hooks/mutations/useDeleteJaneRecord.ts
@@ -3,13 +3,24 @@ import { JaneID } from "@/types/JaneType";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useDeleteJaneRecord() {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: async (rows: JaneID[]) => {
       const ids = rows.map((entry) => entry.id);
       await deleteJaneByIds(ids);
     },
-    onSuccess: () => {
-      const queryClient = useQueryClient();
+    onMutate: async (rows) => {
+      await queryClient.cancelQueries({ queryKey: ["janeData"] });
+
+      // optimistic update
+      queryClient.setQueryData<JaneID[]>(["janeData"], old => old?.filter(d => !rows.map(x => x.id).includes(d.id)) ?? [])
+    },
+    onError: (err) => {
+      console.error("failed to delete jane records:");
+      console.error(err);
+    },
+    onSettled: () => {
       queryClient.invalidateQueries({ queryKey: ["janeData"] });
     },
   });
