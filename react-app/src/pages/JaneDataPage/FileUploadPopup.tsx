60d191bd9ce340914d9d3f385ebd7bcb2f56a1e2
Sophie Tsai <stsai123@umd.edu>
2025-09-25 14:25:02 -0400

    Modify file upload styling for consistency

--- react-app/src/pages/JaneDataPage/FileUploadPopup.tsx
+++ react-app/src/pages/JaneDataPage/FileUploadPopup.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import Modal from "../../components/Modal";
 import { FiCheckCircle } from "react-icons/fi";
+import { IoIosClose } from "react-icons/io";
 import { Tooltip } from "react-tooltip";
 import apptUploadIcon from "../../assets/apptUpload.svg";
 import clientUploadIcon from "../../assets/clientUpload.svg";
@@ -12,8 +13,11 @@ type FileUploadPopupProps = {
   onSubmit: (apptFile: File | null, clientFile: File | null) => void;
 };
 
-const FileUploadPopup = ({ isOpen, onClose, onSubmit }: FileUploadPopupProps) => {
-
+const FileUploadPopup = ({
+  isOpen,
+  onClose,
+  onSubmit,
+}: FileUploadPopupProps) => {
   const resetState = () => {
     setApptFile(null);
     setClientFile(null);
@@ -27,9 +31,14 @@ const FileUploadPopup = ({ isOpen, onClose, onSubmit }: FileUploadPopupProps) =>
 
   const [apptFile, setApptFile] = useState<File | null>(null);
   const [clientFile, setClientFile] = useState<File | null>(null);
-  const [errorType, setErrorType] = useState<"none" | "invalidType" | "missingClients">("none");
+  const [errorType, setErrorType] = useState<
+    "none" | "invalidType" | "missingClients"
+  >("none");
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>, type: "appt" | "client") => {
+  const handleFileChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    type: "appt" | "client"
+  ) => {
     const file = e.target.files?.[0];
     if (!file) return;
 
@@ -57,28 +66,44 @@ const FileUploadPopup = ({ isOpen, onClose, onSubmit }: FileUploadPopupProps) =>
 
   return (
     <Modal open={isOpen} onClose={handleClose} height={350} width={500}>
-      <div className="w-[500px] h-[350px] p-5 rounded-xl border-[1.5px]">
-
-        <div className="flex justify-between items-center mb-3">
-          <h2 className="text-lg font-normal text-[36px] leading-[20px] tracking-[0.01em] text-black">Jane File Upload</h2>
-          <button onClick={handleClose}>&times;</button>
+      <div className="flex flex-col h-full">
+        <div className="flex justify-between items-center m-2">
+          <p className="text-lg">Jane File Upload</p>
+          <button
+            onClick={() => {
+              handleClose();
+            }}
+            className="absolute top-0.25 right-0.25 text-bcgw-blue-dark hover:text-gray-600 cursor-pointer">
+            <IoIosClose size={40} />
+          </button>
         </div>
+        <div className="w-full h-[1.5px] bg-black" />
 
-        <hr className="-mx-5 mt-1 mb-3 border-t border-black/75" />
-
-        <p className="text-[16px] text-gray-500 mb-4">
+        <p className="text-gray-500 m-3">
           <span className="text-red-600">*</span> Appointment sheet is required
         </p>
 
-        <div className="grid grid-cols-2 gap-4 place-items-center mt-8 mb-4">
-
+        <div className="flex flex-row justify-evenly place-items-center mt-4 mb-4">
           <div className="flex flex-col items-center">
-            <label htmlFor="appt-upload" className="cursor-pointer flex flex-col items-center">
-              <img src={apptUploadIcon} alt="Upload Appts" className="w-14 h-14" />
-              <span className="mt-2 text-[13px] font-medium underline underline-offset-2 decoration-1 text-gray-900">
-                <span className="text-red-600">*</span> UPLOAD APPTS
+            <label
+              htmlFor="appt-upload"
+              className="cursor-pointer flex flex-col items-center">
+              <img
+                src={apptUploadIcon}
+                alt="Upload Appts"
+                className="w-15 h-15 ml-3"
+              />
+              <span className="mt-2 text-sm font-medium">
+                <span className="text-red-600">* </span>
+                <span className="underline underline-offset-2 decoration-1">
+                  UPLOAD APPTS
+                </span>
                 {apptFile && (
-                  <FiCheckCircle className="inline-block align-middle ml-1" size={18} color="#04BB22" />
+                  <FiCheckCircle
+                    className="inline-block align-middle ml-1"
+                    size={18}
+                    color="#04BB22"
+                  />
                 )}
               </span>
             </label>
@@ -92,12 +117,22 @@ const FileUploadPopup = ({ isOpen, onClose, onSubmit }: FileUploadPopupProps) =>
           </div>
 
           <div className="flex flex-col items-center mb-2">
-            <label htmlFor="client-upload" className="cursor-pointer flex flex-col items-center">
-              <img src={clientUploadIcon} alt="Upload Clients" className="w-14 h-14" />
-              <span className="mt-2 text-[13px] font-medium underline underline-offset-2 decoration-1 text-gray-900">
+            <label
+              htmlFor="client-upload"
+              className="cursor-pointer flex flex-col items-center">
+              <img
+                src={clientUploadIcon}
+                alt="Upload Clients"
+                className="w-17 h-17"
+              />
+              <span className="mt-2 text-sm font-medium underline underline-offset-2 decoration-1">
                 UPLOAD CLIENTS
                 {clientFile && (
-                  <FiCheckCircle className="inline-block align-middle ml-1" size={18} color="#04BB22" />
+                  <FiCheckCircle
+                    className="inline-block align-middle ml-1"
+                    size={18}
+                    color="#04BB22"
+                  />
                 )}
               </span>
             </label>
@@ -111,27 +146,26 @@ const FileUploadPopup = ({ isOpen, onClose, onSubmit }: FileUploadPopupProps) =>
           </div>
         </div>
 
-
         {/* Errors */}
         {errorType === "invalidType" && (
-          <p className="text-sm text-red-600">
+          <p className="text-sm text-center mx-4 text-red-600">
             The file(s) you uploaded does not match upload type
           </p>
         )}
         {errorType === "missingClients" && (
-          <>
-            <p className="text-sm text-red-600">
-              Some clients in the uploaded appointment sheet are not in the system. Please upload client sheet with new clients.{" "}
+          <div className="flex flex-col items-center">
+            <p className="text-sm text-center mx-4 text-red-600">
+              Some clients in the uploaded appointment sheet are not in the
+              system. Please upload client sheet with new clients.{" "}
               <span
                 data-tooltip-id="missingClientsTip"
-                className="underline cursor-pointer"
-              >
+                className="underline cursor-pointer">
                 View missing clients
               </span>
             </p>
 
             <Tooltip id="missingClientsTip" place="bottom">
-              <div className="text-sm">
+              <div className="text-sm text-center">
                 <div>Bob Bobby</div>
                 <div>Bob Bobby</div>
                 <div>Bob Bobby</div>
@@ -140,25 +174,24 @@ const FileUploadPopup = ({ isOpen, onClose, onSubmit }: FileUploadPopupProps) =>
                 <div>...</div>
               </div>
             </Tooltip>
-          </>
+          </div>
         )}
 
         <div className="flex justify-center mt-6">
           <button
-            className={`px-6 py-2 rounded ${uploadButtonEnabled
-              ? "bg-bcgw-yellow-dark hover:bg-bcgw-yellow-light"
-              : "bg-gray-300 cursor-not-allowed"
-              }`}
+            className={`px-6 py-2 rounded-lg border border-black ${
+              uploadButtonEnabled
+                ? "bg-bcgw-yellow-dark hover:bg-bcgw-yellow-light"
+                : "bg-gray-300 cursor-not-allowed"
+            }`}
             disabled={!uploadButtonEnabled}
-            onClick={handleSubmit}
-          >
+            onClick={handleSubmit}>
             UPLOAD DATA
           </button>
         </div>
       </div>
-    </Modal >
+    </Modal>
   );
 };
 
 export default FileUploadPopup;
-
