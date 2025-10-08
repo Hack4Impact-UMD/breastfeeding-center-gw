fa681a4d92608952c330ea197a7570172d572b1f
Jessica Hoang <jessicahoang712@gmail.com>
2025-10-08 16:11:45 -0400

    client parsing csv

--- cloud-functions/functions/src/utils/janeUploadClients.ts
+++ cloud-functions/functions/src/utils/janeUploadClients.ts
@@ -1,91 +1,89 @@
-// import * as XLSX from "xlsx";
-// import { Jane, VisitType } from "../types/JaneType";
+import csv from "csvtojson";
+import { Baby, Client } from "../types/ClientType";
 
-// export async function getJaneTypes(
-//   e: React.ChangeEvent<HTMLInputElement>,
-// ): Promise<Jane[]> {
-//   const file = e.target.files![0];
-//   const data = await file.arrayBuffer();
-//   const workbook = XLSX.read(data);
-//   const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+export async function parseClientSheet(
+  fileName: string,
+  fileAsBuffer: Buffer<ArrayBufferLike>,
+) {
+  const parts = fileName.split(".");
+  let isCsv = false;
 
-//   const jsonData = XLSX.utils.sheet_to_json(worksheet, {
-//     header: 1,
-//     raw: true,
-//   });
+  let extension = parts.length > 1 ? parts.pop()!.toLowerCase() : "";
+  if (extension === "csv") {
+    isCsv = true;
+  } else if (extension !== "xlsx") {
+    return "error not csv/xlsx";
+  }
 
-//   const headers: string[] = jsonData[0] as string[];
+  const requiredHeaders = [
+    "patient_number",
+    "First Name",
+    "Middle Name",
+    "Last Name",
+    "Email",
+    "Mobile Phone",
+    "Insurance Company Name",
+    "Preferred Name",
+    "Birth Date",
+  ];
 
-//   const relevantColumns = [
-//     { excel: "id", json: "apptId" },
-//     { excel: "patient_first_name", json: "firstName" },
-//     { excel: "patient_last_name", json: "lastName" },
-//     "treatment_name",
-//     { excel: "insurance_state", json: "insurance" },
-//     { excel: "start_at", json: "date" },
-//   ];
+  let jsonArray = [];
 
-//   const columnIndexes = relevantColumns.map((col) =>
-//     headers.indexOf(typeof col === "string" ? col : col.excel),
-//   );
+  if (isCsv) {
+    const fileAsString = fileAsBuffer.toString();
+    jsonArray = await csv().fromString(fileAsString);
+    const headers = Object.keys(jsonArray[0]);
+    const missing = headers.filter((h: string) => requiredHeaders.includes(h));
+    if (missing.length !== 8) {
+      console.log(missing);
+      return "Missing headers";
+    }
+  } else {
+    // xlsx here
+    // watch out for dob being string or number maybe
+  }
 
-//   const janes: Jane[] = (jsonData.slice(1) as any[]).map((row: string[]) => {
-//     const jane = {} as Jane;
+  // parse raw json data into client type
+  // check if baby or client by looking at preferred name
+  // call appropriate function
+  const clientList: Client[] = [];
+  const babyList: Baby[] = [];
+  jsonArray.forEach((jsonObj) => {
+    if (jsonObj["Preferred Name"].toLowerCase().includes("baby")) {
+      babyList.push(parseBaby(jsonObj));
+    } else {
+      clientList.push(parseClient(jsonObj));
+    }
+  });
 
-//     relevantColumns.forEach((column, index) => {
-//       const columnName = typeof column === "string" ? column : column.json;
-//       (jane as any)[columnName] = row[columnIndexes[index]] || "";
+  return [clientList, babyList];
+}
 
-//       if (columnName === "date") {
-//         const value = (jane as any)[columnName];
-//         console.log(value, typeof value);
+function parseClient(clientRawData: any) {
+  const client = {} as Client;
+  client.id = clientRawData["Patient Number"].trim();
+  client.email = clientRawData.Email.trim();
+  client.firstName = clientRawData["First Name"].trim();
+  client.lastName = clientRawData["Last Name"].trim();
+  if (clientRawData["Middle Name"].length !== 0) {
+    client.middleName = clientRawData["Middle Name"];
+  }
+  if (clientRawData["Insurance Company Name"].length !== 0) {
+    client.insurance = clientRawData["Insurance Company Name"];
+  }
+  return client;
+}
 
-//         if (typeof value === "number") {
-//           // Convert Excel serial number to JS Date
-//           const date = new Date(Math.round((value - 25569) * 86400 * 1000));
-//           // Format it as ISO or however you want
-//           (jane as any)[columnName] = date.toISOString();
-//         } else if (typeof value === "string" && value.trim() !== "") {
-//           // If Excel or XLSX already parsed it into a string
-//           const jsDate = new Date(value);
-//           (jane as any)[columnName] = jsDate.toISOString();
-//         }
-//         console.log(jane.date);
-//       }
-//     });
+function parseBaby(babyRawData: any) {
+  const baby = {} as Baby;
+  baby.id = babyRawData["Patient Number"].trim();
+  baby.dob = new Date(babyRawData["Birth Date"].trim()).toISOString();
+  baby.firstName = babyRawData["First Name"].trim();
+  baby.lastName = babyRawData["Last Name"].trim();
 
-//     // Process visit type and treatment from treatment_name
-//     const fullServiceType = (row[columnIndexes[3]] || "")
-//       .replace(/:/g, "")
-//       .trim();
-//     const serviceLowercase = fullServiceType.toLowerCase();
+  if (babyRawData["Middle Name"].length !== 0) {
+    baby.middleName = babyRawData["Middle Name"];
+  }
 
-//     let visitType: VisitType = "OFFICE"; // default
-//     let treatment = "";
-
-//     if (serviceLowercase.includes("telehealth short")) {
-//       visitType = "TELEHEALTH";
-//       treatment = fullServiceType.substring(17).trim();
-//     } else if (serviceLowercase.includes("telehealth")) {
-//       visitType = "TELEHEALTH";
-//       treatment = fullServiceType.substring(11).trim();
-//     } else if (serviceLowercase.includes("dc office")) {
-//       visitType = "OFFICE";
-//       treatment = fullServiceType.substring(10).trim();
-//     } else if (serviceLowercase.includes("home visit")) {
-//       visitType = "HOMEVISIT";
-//       treatment = fullServiceType.substring(11).trim();
-//     }
-
-//     jane.visitType = visitType;
-//     jane.treatment = treatment;
-//     delete (jane as any).treatment_name;
-
-//     jane.email = "email@gmail.com"; // hardcoded
-//     jane.babyDob = "01/01/2026"; // hardcoded
-
-//     return jane;
-//   });
-
-//   return janes;
-// }
+  return baby;
+}
