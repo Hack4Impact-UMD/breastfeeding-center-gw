23b30f3ad796ffb104b467dcc2a3712b4fe110e9
Jessica Hoang <jessicahoang712@gmail.com>
2025-10-10 01:30:39 -0400

    added error handling, cleaned up parsing

--- cloud-functions/functions/src/routes/janeRoutes.ts
+++ cloud-functions/functions/src/routes/janeRoutes.ts
@@ -18,223 +18,241 @@ router.post("/upload", upload, async (req: Request, res: Response) => {
   // logger.info(req.files);
 
   if (!req.files) return res.status(400).send("Missing files!");
-  const appointmentParseResults = await parseAppointmentSheet(
-    req.files["appointments"][0].name,
-    req.files["appointments"][0].buffer,
-  );
 
-  if (typeof appointmentParseResults == "string") {
-    throw new Error(appointmentParseResults);
-  }
+  try {
+    // confirm received files are of valid type
+    const appointmentFileType = getFileType(req.files["appointments"][0].name);
 
-  const { appointments: appointments_sheet, patientNames } =
-    appointmentParseResults;
+    let clientFileExists = false;
+    if (req.files?.["clients"]?.[0]) {
+      clientFileExists = true;
+    }
+    const clientFileType = getFileType(req.files["clients"][0].name);
 
-  let clientFileExists = false;
-  let clients_sheet!: Client[];
-  let babyList!: Baby[];
-
-  if (req.files?.["clients"]?.[0]) {
-    clientFileExists = true;
-  }
-
-  if (clientFileExists) {
-    const clientParseResults = await parseClientSheet(
-      req.files["clients"][0].name,
-      req.files["clients"][0].buffer,
+    const appointmentParseResults = await parseAppointmentSheet(
+      appointmentFileType,
+      req.files["appointments"][0].buffer,
     );
-    if (typeof clientParseResults == "string") {
-      throw new Error(clientParseResults);
-    }
 
-    clients_sheet = clientParseResults.clientList;
-    babyList = clientParseResults.babyList;
-  }
+    const { appointments: appointments_sheet, patientNames } =
+      appointmentParseResults;
 
-  const appointments_map = new Map<[string, string], JaneAppt[]>();
-
-  appointments_sheet.forEach((jane_appt: JaneAppt) => {
-    const appt_group = appointments_map.get([
-      jane_appt.startAt,
-      jane_appt.clinician,
-    ]);
-    if (appt_group) {
-      appt_group.push(jane_appt);
-    } else {
-      appointments_map.set(
-        [jane_appt.startAt, jane_appt.clinician],
-        [jane_appt],
+    let clients_sheet!: Client[];
+    let babyList!: Baby[];
+    if (clientFileExists) {
+      const clientParseResults = await parseClientSheet(
+        clientFileType,
+        req.files["clients"][0].buffer,
       );
+
+      clients_sheet = clientParseResults.clientList;
+      babyList = clientParseResults.babyList;
     }
-  });
+    console.log(babyList[0].middleName);
 
-  const missing_clients: string[] = [];
+    const appointments_map = new Map<[string, string], JaneAppt[]>();
 
-  function is_baby_appt(appt: JaneAppt): boolean {
-    if (babyList?.some((baby: { id: string }) => baby.id === appt.patientId)) {
+    appointments_sheet.forEach((jane_appt: JaneAppt) => {
+      const appt_group = appointments_map.get([
+        jane_appt.startAt,
+        jane_appt.clinician,
+      ]);
+      if (appt_group) {
+        appt_group.push(jane_appt);
+      } else {
+        appointments_map.set(
+          [jane_appt.startAt, jane_appt.clinician],
+          [jane_appt],
+        );
+      }
+    });
+
+    const missing_clients: string[] = [];
+
+    for (const appointments of appointments_map.values()) {
+      let parent = null; // Client type
+      const babies: Baby[] = []; // list of baby type
+      let parentAppt = null; // JaneAppt type, the parent's appointment
+
+      let parentApptExistsInFirebase = false; // do we already have the parent appointment in firebase?
+
+      for (const appt of appointments) {
+        // the appt is for baby
+        if (is_baby_appt(appt)) {
+          const baby = babyList.find(
+            (baby: { id: string }) => baby.id === appt.patientId,
+          ); // find matching baby
+          if (baby) {
+            babies.push(baby);
+          }
+        } else {
+          // the appt is for client
+          if (await appt_in_firebase(appt)) {
+            parentApptExistsInFirebase = true;
+            break;
+          }
+
+          // get the client info, either from firebase or the clients sheet if the client is not in the db yet
+          if (await client_in_firebase(appt.patientId)) {
+            parent = await get_client_from_firebase(appt.patientId);
+          } else if (
+            clientFileExists &&
+            client_in_clients_sheet(appt.patientId)
+          ) {
+            const client = clients_sheet.find(
+              (client: { id: string }) => client.id == appt.patientId,
+            );
+            parent = client;
+          } else {
+            const patientName = patientNames[appt.patientId];
+            // if the client is not in firebase or the clients sheet, we cannot add this appointment
+            // get the patient's first and last name and add them to the missing clients list
+            if (patientName) {
+              missing_clients.push(
+                `${patientName["firstName"]} ${patientName["lastName"]}`,
+              );
+            }
+            continue; // skip this appointment
+          }
+          parentAppt = appt;
+        }
+      }
+      if (parentApptExistsInFirebase) {
+        continue;
+      }
+
+      // add to the parent object's babies array using the babies matched with their appointment.
+      // NOTE: only add the new babies if they do not already exist in the parent's baby array (check based on their ids)
+      const parentResolved = await parent;
+
+      // merging parent existing baby list and new baby
+      // this implementation may be inefficient
+      babies.forEach((baby) => {
+        if (
+          !parentResolved?.baby.some(
+            (existingBaby: { id: string }) => existingBaby.id === baby.id,
+          )
+        ) {
+          parentResolved?.baby.push(baby);
+        }
+      });
+
+      if (parentResolved) {
+        await add_to_clients_collection(parentResolved);
+      }
+      if (parentAppt) {
+        await add_to_appts_collection(parentAppt);
+      }
+    }
+
+    // if there are missing clients, return an error response with their names.
+    // their names will be displayed in the tooltip for users so they can reupload those clients.
+    if (missing_clients.length > 0) {
+      logger.error(["Missing clients!", missing_clients]);
+      return res.status(400).json({
+        error: "Missing clients!",
+        details: missing_clients,
+      });
+    }
+
+    return res.status(200).send();
+
+    // helpers
+    function getFileType(fileName: string) {
+      const parts = fileName.split(".");
+      const extension = parts.length > 1 ? parts.pop()!.toLowerCase() : "";
+      if (extension === "csv") {
+        return "csv";
+      } else if (extension !== "xlsx") {
+        throw new Error("Incorrect file type");
+      }
+
+      return "xlsx";
+    }
+    function is_baby_appt(appt: JaneAppt): boolean {
+      if (
+        babyList?.some((baby: { id: string }) => baby.id === appt.patientId)
+      ) {
+        return true;
+      }
+      return false;
+    }
+
+    async function appt_in_firebase(appt: JaneAppt): Promise<boolean> {
+      // check if appt in firebase JaneAppt collection
+      const querySnapshot = await db
+        .collection("JaneAppt")
+        .where("apptId", "==", appt.apptId)
+        .get();
+
+      if (querySnapshot.docs.length == 0) {
+        logger.info(
+          `No matching appointment in JaneAppt collection for appointment: ${appt.apptId}`,
+        );
+        return false;
+      }
       return true;
     }
-    return false;
-  }
 
-  async function appt_in_firebase(appt: JaneAppt): Promise<boolean> {
-    // check if appt in firebase JaneAppt collection
-    const querySnapshot = await db
-      .collection("JaneAppt")
-      .where("apptId", "==", appt.apptId)
-      .get();
+    async function client_in_firebase(patientId: string): Promise<boolean> {
+      // check patientid in firebase client collection
+      const querySnapshot = await db
+        .collection("Client")
+        .where("id", "==", patientId)
+        .get();
 
-    if (querySnapshot.docs.length == 0) {
-      logger.info(
-        `No matching appointment in JaneAppt collection for appointment: ${appt.apptId}`,
-      );
-      return false;
-    }
-    return true;
-  }
-
-  async function client_in_firebase(patientId: string): Promise<boolean> {
-    // check patientid in firebase client collection
-    const querySnapshot = await db
-      .collection("Client")
-      .where("id", "==", patientId)
-      .get();
-
-    if (querySnapshot.docs.length == 0) {
-      logger.info(
-        `No matching appointment in Client collection for client ID: ${patientId}`,
-      );
-      return false;
-    }
-    return true;
-  }
-
-  async function add_to_clients_collection(parent: Client) {
-    await db.collection("Client").doc(parent.id).set(parent);
-  }
-
-  async function add_to_appts_collection(appt: JaneAppt) {
-    await db.collection("JaneAppt").doc(appt.apptId).set(appt);
-  }
-
-  async function get_client_from_firebase(patientId: string): Promise<Client> {
-    const querySnapshot = await db
-      .collection("Client")
-      .where("id", "==", patientId)
-      .get();
-
-    // querySnapshot is guaranteed to not be empty
-    const doc = querySnapshot.docs[0];
-    const data = doc.data();
-
-    const client: Client = {
-      id: data.id,
-      firstName: data.firstName,
-      ...(data.middleName && { middleName: data.middleName }),
-      lastName: data.lastName,
-      email: data.email,
-      ...(data.phone && { phone: data.phone }),
-      ...(data.insurance && { insurance: data.insurance }),
-      ...(data.paysimpleId && { paysimpleId: data.paysimpleId }),
-      baby: data.baby,
-    };
-
-    return client;
-  }
-
-  function client_in_clients_sheet(patientId: string): boolean {
-    return clients_sheet.some(
-      (client: { id: string }) => client.id === patientId,
-    );
-  }
-
-  for (const appointments of appointments_map.values()) {
-    let parent = null; // Client type
-    const babies: Baby[] = []; // list of baby type
-    let parentAppt = null; // JaneAppt type, the parent's appointment
-
-    let parentApptExistsInFirebase = false; // do we already have the parent appointment in firebase?
-
-    for (const appt of appointments) {
-      // the appt is for baby
-      if (is_baby_appt(appt)) {
-        const baby = babyList.find(
-          (baby: { id: string }) => baby.id === appt.patientId,
-        ); // find matching baby
-        if (baby) {
-          babies.push(baby);
-        }
-      } else {
-        // the appt is for client
-        if (await appt_in_firebase(appt)) {
-          parentApptExistsInFirebase = true;
-          break;
-        }
-
-        // get the client info, either from firebase or the clients sheet if the client is not in the db yet
-        if (await client_in_firebase(appt.patientId)) {
-          parent = await get_client_from_firebase(appt.patientId);
-        } else if (
-          clientFileExists &&
-          client_in_clients_sheet(appt.patientId)
-        ) {
-          const client = clients_sheet.find(
-            (client: { id: string }) => client.id == appt.patientId,
-          );
-          parent = client;
-        } else {
-          const patientName = patientNames[appt.patientId];
-          // if the client is not in firebase or the clients sheet, we cannot add this appointment
-          // get the patient's first and last name and add them to the missing clients list
-          if (patientName) {
-            missing_clients.push(
-              `${patientName["firstName"]} ${patientName["lastName"]}`,
-            );
-          }
-          continue; // skip this appointment
-        }
-        parentAppt = appt;
+      if (querySnapshot.docs.length == 0) {
+        logger.info(
+          `No matching appointment in Client collection for client ID: ${patientId}`,
+        );
+        return false;
       }
-    }
-    if (parentApptExistsInFirebase) {
-      continue;
+      return true;
     }
 
-    // add to the parent object's babies array using the babies matched with their appointment.
-    // NOTE: only add the new babies if they do not already exist in the parent's baby array (check based on their ids)
-    const parentResolved = await parent;
-
-    // merging parent existing baby list and new baby
-    // this implementation may be inefficient
-    babies.forEach((baby) => {
-      if (
-        !parentResolved?.baby.some(
-          (existingBaby: { id: string }) => existingBaby.id === baby.id,
-        )
-      ) {
-        parentResolved?.baby.push(baby);
-      }
-    });
-
-    if (parentResolved) {
-      await add_to_clients_collection(parentResolved);
+    async function add_to_clients_collection(parent: Client) {
+      await db.collection("Client").doc(parent.id).set(parent);
     }
-    if (parentAppt) {
-      await add_to_appts_collection(parentAppt);
+
+    async function add_to_appts_collection(appt: JaneAppt) {
+      await db.collection("JaneAppt").doc(appt.apptId).set(appt);
     }
+
+    async function get_client_from_firebase(
+      patientId: string,
+    ): Promise<Client> {
+      const querySnapshot = await db
+        .collection("Client")
+        .where("id", "==", patientId)
+        .get();
+
+      // querySnapshot is guaranteed to not be empty
+      const doc = querySnapshot.docs[0];
+      const data = doc.data();
+
+      const client: Client = {
+        id: data.id,
+        firstName: data.firstName,
+        ...(data.middleName && { middleName: data.middleName }),
+        lastName: data.lastName,
+        email: data.email,
+        ...(data.phone && { phone: data.phone }),
+        ...(data.insurance && { insurance: data.insurance }),
+        ...(data.paysimpleId && { paysimpleId: data.paysimpleId }),
+        baby: data.baby,
+      };
+
+      return client;
+    }
+
+    function client_in_clients_sheet(patientId: string): boolean {
+      return clients_sheet.some(
+        (client: { id: string }) => client.id === patientId,
+      );
+    }
+  } catch (e) {
+    return res.status(400).send((e as Error).message);
   }
-
-  // if there are missing clients, return an error response with their names.
-  // their names will be displayed in the tooltip for users so they can reupload those clients.
-  if (missing_clients.length > 0) {
-    logger.error(["Missing clients!", missing_clients]);
-    return res.status(400).json({
-      error: "Missing clients!",
-      details: missing_clients,
-    });
-  }
-
-  return res.status(200).send();
 });
 
 export default router;
--- cloud-functions/functions/src/utils/janeUploadAppts.ts
+++ cloud-functions/functions/src/utils/janeUploadAppts.ts
@@ -3,19 +3,9 @@ import csv from "csvtojson";
 import { JaneAppt, VisitType } from "../types/janeType";
 
 export async function parseAppointmentSheet(
-  fileName: string,
+  fileType: string,
   fileAsBuffer: Buffer<ArrayBufferLike>,
 ) {
-  const parts = fileName.split(".");
-  let isCsv = false;
-
-  const extension = parts.length > 1 ? parts.pop()!.toLowerCase() : "";
-  if (extension === "csv") {
-    isCsv = true;
-  } else if (extension !== "xlsx") {
-    return "error not csv/xlsx";
-  }
-
   const requiredHeaders = [
     "id",
     "patient_number",
@@ -29,6 +19,7 @@ export async function parseAppointmentSheet(
   ];
 
   let jsonArray = [];
+  const isCsv = fileType === "csv";
 
   if (isCsv) {
     const fileAsString = fileAsBuffer.toString();
@@ -36,8 +27,7 @@ export async function parseAppointmentSheet(
     const headers = Object.keys(jsonArray[0]);
     const missing = headers.filter((h: string) => requiredHeaders.includes(h));
     if (missing.length !== 9) {
-      console.log(missing);
-      return "Missing headers";
+      throw new Error("Missing headers");
     }
   } else {
     const workbook = XLSX.read(fileAsBuffer);
@@ -50,7 +40,7 @@ export async function parseAppointmentSheet(
     const headers: string[] = xlsxDataArray[0] as string[];
     const columnIndices = requiredHeaders.map((col) => headers.indexOf(col));
     if (columnIndices.includes(-1)) {
-      return "Missing headers";
+      throw new Error("Missing headers");
     }
 
     // convert the array of string arrays into array of json objects
@@ -59,18 +49,6 @@ export async function parseAppointmentSheet(
       requiredHeaders.forEach((columnName, idx) => {
         // add key pair value for relevant columns
         (jsonObj as any)[columnName] = data[columnIndices[idx]];
-
-        // need to convert date at all???
-        // if (columnName === "start_at" || columnName === "end_at") {
-        //   // remove the timezone so that ISO string will be consistent later on?
-        //   const value = (jsonObj as any)[columnName];
-        //   if (value.includes("-0400")) {
-        //     (jsonObj as any)[columnName] = data[columnIndices[idx]].slice(
-        //       0,
-        //       -6,
-        //     );
-        //   }
-        // }
       });
       return jsonObj;
     });
@@ -118,8 +96,9 @@ function parseAppointment(appt: any) {
   const serviceLowercase = fullServiceType.toLowerCase();
   // get service from the visit type
   let visitType: VisitType = "OFFICE"; // default
-  let service = "";
+  let service = fullServiceType.trim();
 
+  // NEED TO FIX - maybe find index of the word and then grab whatever is after that. shouldn't be hardcoded
   if (serviceLowercase.includes("telehealth short")) {
     visitType = "TELEHEALTH";
     service = fullServiceType.substring(17).trim();
@@ -139,92 +118,3 @@ function parseAppointment(appt: any) {
 
   return janeAppt;
 }
-
-// export async function getJaneTypes(
-//   e: React.ChangeEvent<HTMLInputElement>,
-// ): Promise<Jane[]> {
-//   const file = e.target.files![0];
-//   const data = await file.arrayBuffer();
-//   const workbook = XLSX.read(data);
-//   const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-
-//   const jsonData = XLSX.utils.sheet_to_json(worksheet, {
-//     header: 1,
-//     raw: true,
-//   });
-
-//   const headers: string[] = jsonData[0] as string[];
-
-//   const relevantColumns = [
-//     { excel: "id", json: "apptId" },
-//     { excel: "patient_first_name", json: "firstName" },
-//     { excel: "patient_last_name", json: "lastName" },
-//     "treatment_name",
-//     { excel: "insurance_state", json: "insurance" },
-//     { excel: "start_at", json: "date" },
-//   ];
-
-//   const columnIndexes = relevantColumns.map((col) =>
-//     headers.indexOf(typeof col === "string" ? col : col.excel),
-//   );
-
-//   const janes: Jane[] = (jsonData.slice(1) as any[]).map((row: string[]) => {
-//     const jane = {} as Jane;
-
-//     relevantColumns.forEach((column, index) => {
-//       const columnName = typeof column === "string" ? column : column.json;
-//       (jane as any)[columnName] = row[columnIndexes[index]] || "";
-
-//       if (columnName === "date") {
-//         const value = (jane as any)[columnName];
-//         console.log(value, typeof value);
-
-//         if (typeof value === "number") {
-//           // Convert Excel serial number to JS Date
-//           const date = new Date(Math.round((value - 25569) * 86400 * 1000));
-//           // Format it as ISO or however you want
-//           (jane as any)[columnName] = date.toISOString();
-//         } else if (typeof value === "string" && value.trim() !== "") {
-//           // If Excel or XLSX already parsed it into a string
-//           const jsDate = new Date(value);
-//           (jane as any)[columnName] = jsDate.toISOString();
-//         }
-//         console.log(jane.date);
-//       }
-//     });
-
-//     // Process visit type and treatment from treatment_name
-//     const fullServiceType = (row[columnIndexes[3]] || "")
-//       .replace(/:/g, "")
-//       .trim();
-//     const serviceLowercase = fullServiceType.toLowerCase();
-
-//     let visitType: VisitType = "OFFICE"; // default
-//     let treatment = "";
-
-//     if (serviceLowercase.includes("telehealth short")) {
-//       visitType = "TELEHEALTH";
-//       treatment = fullServiceType.substring(17).trim();
-//     } else if (serviceLowercase.includes("telehealth")) {
-//       visitType = "TELEHEALTH";
-//       treatment = fullServiceType.substring(11).trim();
-//     } else if (serviceLowercase.includes("dc office")) {
-//       visitType = "OFFICE";
-//       treatment = fullServiceType.substring(10).trim();
-//     } else if (serviceLowercase.includes("home visit")) {
-//       visitType = "HOMEVISIT";
-//       treatment = fullServiceType.substring(11).trim();
-//     }
-
-//     jane.visitType = visitType;
-//     jane.treatment = treatment;
-//     delete (jane as any).treatment_name;
-
-//     jane.email = "email@gmail.com"; // hardcoded
-//     jane.babyDob = "01/01/2026"; // hardcoded
-
-//     return jane;
-//   });
-
-//   return janes;
-// }
