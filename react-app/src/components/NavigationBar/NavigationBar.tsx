1a218e60c9e705ed1049a613758f57ca5bf97286
Ramy Kaddouri <myramkadd@gmail.com>
2025-10-23 13:29:27 -0400

    fix mobile user management link

--- react-app/src/components/NavigationBar/NavigationBar.tsx
+++ react-app/src/components/NavigationBar/NavigationBar.tsx
@@ -48,9 +48,8 @@ const NavigationBar: React.FC<NavigationBarProps> = ({
 
   return (
     <div
-      className={`flex flex-col justify-left h-screen fixed bg-[#F9F8F8] shadow-[4px_4px_4px_0px_rgba(0,_0,_0,_0.25)] transition-all duration-200 ease-in-out ${
-        navBarOpen ? "w-[250px]" : "w-[60px]"
-      }`}
+      className={`flex flex-col justify-left h-screen fixed bg-[#F9F8F8] shadow-[4px_4px_4px_0px_rgba(0,_0,_0,_0.25)] transition-all duration-200 ease-in-out ${navBarOpen ? "w-[250px]" : "w-[60px]"
+        }`}
     >
       <div className="flex flex-col h-full ">
         <div className="flex flex-row justify-between items-center pr-3 pt-1">
@@ -237,7 +236,7 @@ const NavigationBar: React.FC<NavigationBarProps> = ({
               </NavLink>
 
               <NavLink
-                to="/management"
+                to="/user-management"
                 className={({ isActive }) =>
                   isActive ? activeIconStyle : notActiveIconStyle
                 }
