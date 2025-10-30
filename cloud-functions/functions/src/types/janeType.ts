032d92e51e73e1165141e43cfc80c082bf32179a
Ramy Kaddouri <myramkadd@gmail.com>
2025-10-29 20:52:54 -0400

    rm duplicate JaneAppt decl

--- cloud-functions/functions/src/types/janeType.ts
+++ cloud-functions/functions/src/types/janeType.ts
@@ -26,14 +26,3 @@ export interface JaneAppt {
 }
 
 export type VisitType = "HOMEVISIT" | "OFFICE" | "TELEHEALTH";
-
-export interface JaneAppt {
-  apptId: string; // appt_id
-  patientId: string; // patient_number
-  startAt: string; // start_at ISO
-  endAt: string; // end_at ISO
-  visitType: VisitType; // treatment_name
-  service: string; // treatment_name
-  clinician: string; // staff_member
-  firstVisit: boolean;
-}
