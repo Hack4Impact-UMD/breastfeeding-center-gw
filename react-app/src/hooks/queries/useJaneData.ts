4b308aa3e5132014adc9595d185099ef7522f455
Ramy Kaddouri <myramkadd@gmail.com>
2025-10-29 23:29:36 -0400

    rm multer dep

--- react-app/src/hooks/queries/useJaneData.ts
+++ react-app/src/hooks/queries/useJaneData.ts
@@ -45,6 +45,7 @@ export function useJaneData(startDate?: string, endDate?: string) {
           }
         }),
       );
+      console.log("TOTAL ROWS: " + janeTableRows.length)
       return janeTableRows;
     },
   });
