33ee4ac1338042e0ae102e844927555bae0c3ce4
patrickhong <path.plh@gmail.com>
2025-11-08 00:19:11 -0500

    acuity appts in range function

--- cloud-functions/functions/src/services/acuity.ts
+++ cloud-functions/functions/src/services/acuity.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { config } from "../config";
+import { DateTime } from "luxon";
 
 export const acuityClient = () => {
   const creds = {
@@ -14,3 +15,37 @@ export const acuityClient = () => {
     },
   });
 };
+
+export async function getAllAcuityApptsInRange(
+  startDate: string,
+  endDate: string,
+) {
+  const startDateLuxon = DateTime.fromISO(startDate);
+  const endDateLuxon = DateTime.fromISO(endDate);
+
+  let current = startDateLuxon;
+  const dates: DateTime[] = [];
+
+  while (current <= endDateLuxon) {
+    dates.push(current);
+    current = current.plus({ months: 1 });
+  }
+  dates.push(endDateLuxon);
+  const api = acuityClient();
+
+  let acuityApptsInRange = [];
+
+  for (let i = 0; i < dates.length - 1; i++) {
+    // need to verify the type of response this is
+    const response = await api.get("/appointments", {
+      params: {
+        max: -1,
+        maxDate: dates[i + 1].toISO(),
+        minDate: dates[i].toISO(),
+      },
+    });
+    acuityApptsInRange = [...acuityApptsInRange, ...response];
+  }
+
+  return acuityApptsInRange;
+}
