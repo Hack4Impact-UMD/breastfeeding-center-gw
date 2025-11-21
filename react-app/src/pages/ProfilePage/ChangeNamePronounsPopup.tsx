26123e972ce2c8fb5511a0eeaf7ca00d8f3a28c4
Sophie Tsai <sophietsai31@gmail.com>
2025-11-21 16:39:22 -0500

    Update Pronouns dropdown on pfp

--- react-app/src/pages/ProfilePage/ChangeNamePronounsPopup.tsx
+++ react-app/src/pages/ProfilePage/ChangeNamePronounsPopup.tsx
@@ -2,6 +2,21 @@ import { useState } from "react";
 import { IoIosClose } from "react-icons/io";
 import Modal from "../../components/Modal";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { ChevronsUpDownIcon } from "lucide-react";
 
 const ChangeNamePronounsPopup = ({
   open,
@@ -19,13 +34,22 @@ const ChangeNamePronounsPopup = ({
   const [firstName, setFirstName] = useState(initialFirstName);
   const [lastName, setLastName] = useState(initialLastName);
   const [pronouns, setPronouns] = useState(initialPronouns);
+  const [isPronounDropdownOpen, setIsPronounDropdownOpen] = useState(false);
+  const [pronounQuery, setPronounQuery] = useState("");
 
   const pronounOptions = [
-    "She/Her/Hers",
-    "He/Him/His",
-    "They/Them/Theirs",
-    "Other",
-    "None",
+    {
+      value: "She/Her/Hers",
+      label: "She/Her/Hers",
+    },
+    {
+      value: "He/Him/His",
+      label: "He/Him/His",
+    },
+    {
+      value: "They/Them/Theirs",
+      label: "They/Them/Theirs",
+    },
   ];
 
   const handleSave = () => {
@@ -53,12 +77,10 @@ const ChangeNamePronounsPopup = ({
     <Modal open={open} onClose={onClose} height={350} width={600}>
       <div className="w-full">
         <div className="flex flex-col bg-white rounded-2xl w-full h-auto overflow-y-auto sm:overflow-visible">
-
           <ModalHeader onClose={onClose} />
 
           {/* CONTENT WRAPPER – now mobile responsive */}
           <div className="flex flex-col m-4 sm:m-8 mb-2 text-left space-y-4">
-
             {/* FIRST NAME */}
             <div className="flex flex-col sm:grid sm:grid-cols-[150px_1fr] sm:gap-x-2">
               <label className="text-sm sm:text-base font-medium mb-1 sm:mb-0 sm:content-center">
@@ -92,17 +114,96 @@ const ChangeNamePronounsPopup = ({
               <label className="text-sm sm:text-base font-medium mb-1 sm:mb-0 sm:content-center">
                 Pronouns
               </label>
-              <select
-                value={pronouns}
-                onChange={(e) => setPronouns(e.target.value)}
-                className="w-full sm:w-fit border-[1.5px] border-black px-3 py-2 bg-white cursor-pointer rounded-2xl text-sm sm:text-base"
+              <Popover
+                open={isPronounDropdownOpen}
+                onOpenChange={setIsPronounDropdownOpen}
               >
-                {pronounOptions.map((option) => (
-                  <option key={option} value={option}>
-                    {option}
-                  </option>
-                ))}
-              </select>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant="outline"
+                    role="combobox"
+                    aria-expanded={isPronounDropdownOpen}
+                    className="w-full sm:w-[175px] justify-between hover:bg-background rounded-2xl mb-4 sm:mb-0"
+                  >
+                    {pronouns ? pronouns : "Select..."}
+                    <ChevronsUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-full sm:w-[175px] p-0 bg-white">
+                  <Command>
+                    <CommandInput
+                      placeholder="Search/Add..."
+                      value={pronounQuery}
+                      onValueChange={setPronounQuery}
+                    />
+                    <CommandList>
+                      <CommandGroup>
+                        {pronounOptions.map((pronounOption) => (
+                          <CommandItem
+                            className={cn(
+                              "cursor-pointer",
+                              pronouns === pronounOption.value
+                                ? "bg-bcgw-yellow-dark"
+                                : "hover:bg-bcgw-yellow-light",
+                            )}
+                            key={pronounOption.value}
+                            value={pronounOption.value}
+                            onSelect={(currentValue) => {
+                              setPronouns(
+                                currentValue === pronouns ? "" : currentValue,
+                              );
+                              setIsPronounDropdownOpen(false);
+                            }}
+                          >
+                            {pronounOption.label}
+                          </CommandItem>
+                        ))}
+                        {pronouns !== "" &&
+                          !pronounOptions.some(
+                            (o) =>
+                              o.value.toLowerCase() === pronouns.toLowerCase(),
+                          ) && (
+                            <CommandItem
+                              className={cn(
+                                "cursor-pointer bg-bcgw-yellow-dark",
+                              )}
+                              key={pronouns}
+                              value={pronouns}
+                              onSelect={() => {
+                                setPronouns("");
+                                setIsPronounDropdownOpen(false);
+                              }}
+                            >
+                              {pronouns}
+                            </CommandItem>
+                          )}
+                        {pronounQuery.trim() !== "" &&
+                          !pronounOptions.some(
+                            (o) =>
+                              o.value.toLowerCase() ===
+                              pronounQuery.toLowerCase(),
+                          ) && (
+                            <CommandItem
+                              className={cn(
+                                "cursor-pointer",
+                                "hover:bg-bcgw-yellow-light",
+                              )}
+                              key={pronounQuery}
+                              value={pronounQuery}
+                              onSelect={() => {
+                                setPronouns(pronounQuery);
+                                setIsPronounDropdownOpen(false);
+                                setPronounQuery("");
+                              }}
+                            >
+                              + {pronounQuery}
+                            </CommandItem>
+                          )}
+                      </CommandGroup>
+                    </CommandList>
+                  </Command>
+                </PopoverContent>
+              </Popover>
             </div>
           </div>
 
