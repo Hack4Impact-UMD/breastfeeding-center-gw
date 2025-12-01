5ab25852905883dedffae12f96bb5b652e4aa5c9
Annie Nguyen <lunagod4421@gmail.com>
2025-12-01 14:21:04 -0500

    changing name for key to appts, client, email

--- react-app/src/queries/acuityQueries.ts
+++ react-app/src/queries/acuityQueries.ts
@@ -1,6 +1,6 @@
 import { createQueryKeys } from "@lukemorales/query-key-factory";
 
 export const acuityQueries = createQueryKeys("acuityData", {
-    apptsForClient: (email: string) => ["apptsForClient", email],
+    apptsForClient: (email: string) => ["appts", "client", email],
 });
 
