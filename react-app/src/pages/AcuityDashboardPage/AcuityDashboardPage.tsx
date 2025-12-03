f5f5cc1d1db17503775eeac5667825ac7b14b50c
Ramy Kaddouri <myramkadd@gmail.com>
2025-12-03 17:18:55 -0500

    start memoizing acuity data processing

--- react-app/src/pages/AcuityDashboardPage/AcuityDashboardPage.tsx
+++ react-app/src/pages/AcuityDashboardPage/AcuityDashboardPage.tsx
@@ -58,14 +58,14 @@ export default function AcuityDashboardPage() {
   const [selectedPopularityClass, setSelectedPopularityClass] =
     useState("ALL CLASSES");
 
-  const classFilterOptions = [
+  const classFilterOptions = useMemo(() => [
     "ALL CLASSES",
     "POSTPARTUM CLASSES",
     "PRENATAL CLASSES",
     "INFANT MASSAGE",
     "PARENT GROUPS",
     "CHILDBIRTH CLASSES",
-  ];
+  ], []);
   const trimesters = [
     "FIRST TRIM",
     "SECOND TRIM",
@@ -129,156 +129,158 @@ export default function AcuityDashboardPage() {
 
   console.log(allAppointmentData);
 
-  const trimesterAttendance: Map<string, number> = new Map();
-  const classesToCategory: Map<string, string> = new Map();
-  if (filteredAppointmentsForTrimester) {
-    for (const appt of filteredAppointmentsForTrimester) {
-      if (appt.class && appt.classCategory) {
-        classesToCategory.set(appt.class, appt.classCategory);
-      }
+  const [trimesterAttendance, classesToCategory] = useMemo(() => {
+    const trimesterAttendance: Map<string, number> = new Map();
+    const classesToCategory: Map<string, string> = new Map();
+    if (filteredAppointmentsForTrimester) {
+      for (const appt of filteredAppointmentsForTrimester) {
+        if (appt.class && appt.classCategory) {
+          classesToCategory.set(appt.class, appt.classCategory);
+        }
 
-      // skip babies with no due dates
-      if (appt.babyDueDatesISO.length === 0) {
-        continue;
-      }
-      const apptTime = DateTime.fromISO(appt.datetime);
-      // get date that is closest to the appt date
-      let timeDiff = DateTime.fromISO(appt.babyDueDatesISO[0]).diff(
-        apptTime,
-        "weeks",
-      ).weeks;
-      // set chosen baby date as first in array
-      let chosenDate = DateTime.fromISO(appt.babyDueDatesISO[0]);
-      // if there are multiple dates in the array, check for each
-      for (let i = 1; i < appt.babyDueDatesISO.length; i++) {
-        // get difference between curr date and appt time
-        const currDiff = DateTime.fromISO(appt.babyDueDatesISO[i]).diff(
+        // skip babies with no due dates
+        if (appt.babyDueDatesISO.length === 0) {
+          continue;
+        }
+        const apptTime = DateTime.fromISO(appt.datetime);
+        // get date that is closest to the appt date
+        let timeDiff = DateTime.fromISO(appt.babyDueDatesISO[0]).diff(
           apptTime,
           "weeks",
         ).weeks;
-        // if the time difference is less than the curr min, update
-        chosenDate =
-          currDiff < timeDiff
-            ? DateTime.fromISO(appt.babyDueDatesISO[i])
-            : chosenDate;
-        timeDiff = Math.min(timeDiff, currDiff);
-      }
+        // set chosen baby date as first in array
+        let chosenDate = DateTime.fromISO(appt.babyDueDatesISO[0]);
+        // if there are multiple dates in the array, check for each
+        for (let i = 1; i < appt.babyDueDatesISO.length; i++) {
+          // get difference between curr date and appt time
+          const currDiff = DateTime.fromISO(appt.babyDueDatesISO[i]).diff(
+            apptTime,
+            "weeks",
+          ).weeks;
+          // if the time difference is less than the curr min, update
+          chosenDate =
+            currDiff < timeDiff
+              ? DateTime.fromISO(appt.babyDueDatesISO[i])
+              : chosenDate;
+          timeDiff = Math.min(timeDiff, currDiff);
+        }
 
-      // calculate trimester
-      // if the date has already passed, assume that it's a birth date
-      if (chosenDate < apptTime) {
-        const diff = apptTime.diff(chosenDate, "weeks").weeks;
-        // first three months is fourth trimester
-        if (diff <= 12) {
-          trimesterAttendance.set(
-            `${appt.classCategory?.toLowerCase()} FOURTH TRIM`,
-            trimesterAttendance.has(
+        // calculate trimester
+        // if the date has already passed, assume that it's a birth date
+        if (chosenDate < apptTime) {
+          const diff = apptTime.diff(chosenDate, "weeks").weeks;
+          // first three months is fourth trimester
+          if (diff <= 12) {
+            trimesterAttendance.set(
               `${appt.classCategory?.toLowerCase()} FOURTH TRIM`,
-            )
-              ? trimesterAttendance.get(
+              trimesterAttendance.has(
+                `${appt.classCategory?.toLowerCase()} FOURTH TRIM`,
+              )
+                ? trimesterAttendance.get(
                   `${appt.classCategory?.toLowerCase()} FOURTH TRIM`,
                 )! + 1
-              : 1,
-          );
-          trimesterAttendance.set(
-            `${appt.class?.toLowerCase()} FOURTH TRIM`,
-            trimesterAttendance.has(`${appt.class?.toLowerCase()} FOURTH TRIM`)
-              ? trimesterAttendance.get(
+                : 1,
+            );
+            trimesterAttendance.set(
+              `${appt.class?.toLowerCase()} FOURTH TRIM`,
+              trimesterAttendance.has(`${appt.class?.toLowerCase()} FOURTH TRIM`)
+                ? trimesterAttendance.get(
                   `${appt.class?.toLowerCase()} FOURTH TRIM`,
                 )! + 1
-              : 1,
-          );
-        } else {
-          trimesterAttendance.set(
-            `${appt.classCategory?.toLowerCase()} FIFTH TRIM`,
-            trimesterAttendance.has(
+                : 1,
+            );
+          } else {
+            trimesterAttendance.set(
               `${appt.classCategory?.toLowerCase()} FIFTH TRIM`,
-            )
-              ? trimesterAttendance.get(
+              trimesterAttendance.has(
+                `${appt.classCategory?.toLowerCase()} FIFTH TRIM`,
+              )
+                ? trimesterAttendance.get(
                   `${appt.classCategory?.toLowerCase()} FIFTH TRIM`,
                 )! + 1
-              : 1,
-          );
-          trimesterAttendance.set(
-            `${appt.class?.toLowerCase()} FIFTH TRIM`,
-            trimesterAttendance.has(`${appt.class?.toLowerCase()} FIFTH TRIM`)
-              ? trimesterAttendance.get(
+                : 1,
+            );
+            trimesterAttendance.set(
+              `${appt.class?.toLowerCase()} FIFTH TRIM`,
+              trimesterAttendance.has(`${appt.class?.toLowerCase()} FIFTH TRIM`)
+                ? trimesterAttendance.get(
                   `${appt.class?.toLowerCase()} FIFTH TRIM`,
                 )! + 1
-              : 1,
+                : 1,
+            );
+          }
+        } else {
+          // if not, it is a due date
+          // pregnancy typically 40 weeks, find "start" of pregnancy
+          const pregnancyStart = chosenDate.minus({ weeks: 40 });
+          // subtract start date from due date and round
+          const weeksIntoPregnancy = Math.floor(
+            apptTime.diff(pregnancyStart, "weeks").weeks,
           );
-        }
-      } else {
-        // if not, it is a due date
-        // pregnancy typically 40 weeks, find "start" of pregnancy
-        const pregnancyStart = chosenDate.minus({ weeks: 40 });
-        // subtract start date from due date and round
-        const weeksIntoPregnancy = Math.floor(
-          apptTime.diff(pregnancyStart, "weeks").weeks,
-        );
-        if (weeksIntoPregnancy >= 29) {
-          trimesterAttendance.set(
-            `${appt.classCategory?.toLowerCase()} THIRD TRIM`,
-            trimesterAttendance.has(
+          if (weeksIntoPregnancy >= 29) {
+            trimesterAttendance.set(
               `${appt.classCategory?.toLowerCase()} THIRD TRIM`,
-            )
-              ? trimesterAttendance.get(
+              trimesterAttendance.has(
+                `${appt.classCategory?.toLowerCase()} THIRD TRIM`,
+              )
+                ? trimesterAttendance.get(
                   `${appt.classCategory?.toLowerCase()} THIRD TRIM`,
                 )! + 1
-              : 1,
-          );
-          trimesterAttendance.set(
-            `${appt.class?.toLowerCase()} THIRD TRIM`,
-            trimesterAttendance.has(`${appt.class?.toLowerCase()} THIRD TRIM`)
-              ? trimesterAttendance.get(
+                : 1,
+            );
+            trimesterAttendance.set(
+              `${appt.class?.toLowerCase()} THIRD TRIM`,
+              trimesterAttendance.has(`${appt.class?.toLowerCase()} THIRD TRIM`)
+                ? trimesterAttendance.get(
                   `${appt.class?.toLowerCase()} THIRD TRIM`,
                 )! + 1
-              : 1,
-          );
-        } else if (weeksIntoPregnancy >= 15) {
-          // 15-18 is second
-          trimesterAttendance.set(
-            `${appt.classCategory?.toLowerCase()} SECOND TRIM`,
-            trimesterAttendance.has(
+                : 1,
+            );
+          } else if (weeksIntoPregnancy >= 15) {
+            // 15-18 is second
+            trimesterAttendance.set(
               `${appt.classCategory?.toLowerCase()} SECOND TRIM`,
-            )
-              ? trimesterAttendance.get(
+              trimesterAttendance.has(
+                `${appt.classCategory?.toLowerCase()} SECOND TRIM`,
+              )
+                ? trimesterAttendance.get(
                   `${appt.classCategory?.toLowerCase()} SECOND TRIM`,
                 )! + 1
-              : 1,
-          );
-          trimesterAttendance.set(
-            `${appt.class?.toLowerCase()} SECOND TRIM`,
-            trimesterAttendance.has(`${appt.class?.toLowerCase()} SECOND TRIM`)
-              ? trimesterAttendance.get(
+                : 1,
+            );
+            trimesterAttendance.set(
+              `${appt.class?.toLowerCase()} SECOND TRIM`,
+              trimesterAttendance.has(`${appt.class?.toLowerCase()} SECOND TRIM`)
+                ? trimesterAttendance.get(
                   `${appt.class?.toLowerCase()} SECOND TRIM`,
                 )! + 1
-              : 1,
-          );
-        } else {
-          trimesterAttendance.set(
-            `${appt.classCategory?.toLowerCase()} FIRST TRIM`,
-            trimesterAttendance.has(
+                : 1,
+            );
+          } else {
+            trimesterAttendance.set(
               `${appt.classCategory?.toLowerCase()} FIRST TRIM`,
-            )
-              ? trimesterAttendance.get(
+              trimesterAttendance.has(
+                `${appt.classCategory?.toLowerCase()} FIRST TRIM`,
+              )
+                ? trimesterAttendance.get(
                   `${appt.classCategory?.toLowerCase()} FIRST TRIM`,
                 )! + 1
-              : 1,
-          );
-          trimesterAttendance.set(
-            `${appt.class?.toLowerCase()} FIRST TRIM`,
-            trimesterAttendance.has(`${appt.class?.toLowerCase()} FIRST TRIM`)
-              ? trimesterAttendance.get(
+                : 1,
+            );
+            trimesterAttendance.set(
+              `${appt.class?.toLowerCase()} FIRST TRIM`,
+              trimesterAttendance.has(`${appt.class?.toLowerCase()} FIRST TRIM`)
+                ? trimesterAttendance.get(
                   `${appt.class?.toLowerCase()} FIRST TRIM`,
                 )! + 1
-              : 1,
-          );
+                : 1,
+            );
+          }
         }
       }
     }
-  }
-  console.log(trimesterAttendance);
+    return [trimesterAttendance, classesToCategory]
+  }, [filteredAppointmentsForTrimester])
 
   const shouldGroupByWeek = useMemo(() => {
     if (!dateRange?.from || !dateRange?.to) return true;
@@ -383,8 +385,8 @@ export default function AcuityDashboardPage() {
             const date = shouldGroupByWeek
               ? DateTime.fromISO(intervalKey).toJSDate()
               : DateTime.fromFormat(intervalKey, "yyyy-MM")
-                  .startOf("month")
-                  .toJSDate();
+                .startOf("month")
+                .toJSDate();
             const count =
               classAttendanceByInterval
                 .get(intervalKey)
@@ -404,8 +406,8 @@ export default function AcuityDashboardPage() {
             const date = shouldGroupByWeek
               ? DateTime.fromISO(intervalKey).toJSDate()
               : DateTime.fromFormat(intervalKey, "yyyy-MM")
-                  .startOf("month")
-                  .toJSDate();
+                .startOf("month")
+                .toJSDate();
             const count =
               instructorAttendanceByInterval
                 .get(intervalKey)
@@ -564,8 +566,8 @@ export default function AcuityDashboardPage() {
     selectedPopularityClass === "ALL CLASSES"
       ? allInstructorData
       : allInstructorData.filter(
-          (item) => item.key === selectedPopularityClass,
-        );
+        (item) => item.key === selectedPopularityClass,
+      );
 
   const classAttendanceTableExtras = (
     <div className="w-full flex justify-end">
@@ -608,21 +610,19 @@ export default function AcuityDashboardPage() {
           <div className={`${centerItemsInDiv} pt-4`}>
             <div className="flex flex-row">
               <button
-                className={`${graphTableButtonStyle} ${
-                  attendanceDisplay == "graph"
-                    ? "bg-bcgw-gray-light"
-                    : "bg-[#f5f5f5]"
-                }`}
+                className={`${graphTableButtonStyle} ${attendanceDisplay == "graph"
+                  ? "bg-bcgw-gray-light"
+                  : "bg-[#f5f5f5]"
+                  }`}
                 onClick={() => setAttendanceDisplay("graph")}
               >
                 Graph
               </button>
               <button
-                className={`${graphTableButtonStyle} ${
-                  attendanceDisplay == "table"
-                    ? "bg-bcgw-gray-light"
-                    : "bg-[#f5f5f5]"
-                }`}
+                className={`${graphTableButtonStyle} ${attendanceDisplay == "table"
+                  ? "bg-bcgw-gray-light"
+                  : "bg-[#f5f5f5]"
+                  }`}
                 onClick={() => setAttendanceDisplay("table")}
               >
                 Table
@@ -767,21 +767,19 @@ export default function AcuityDashboardPage() {
         <div className={`${centerItemsInDiv} pt-8`}>
           <div className="flex flex-row">
             <button
-              className={`${graphTableButtonStyle} ${
-                popularityDisplay == "graph"
-                  ? "bg-bcgw-gray-light"
-                  : "bg-[#f5f5f5]"
-              }`}
+              className={`${graphTableButtonStyle} ${popularityDisplay == "graph"
+                ? "bg-bcgw-gray-light"
+                : "bg-[#f5f5f5]"
+                }`}
               onClick={() => setPopularityDisplay("graph")}
             >
               Graph
             </button>
             <button
-              className={`${graphTableButtonStyle} ${
-                popularityDisplay == "table"
-                  ? "bg-bcgw-gray-light"
-                  : "bg-[#f5f5f5]"
-              }`}
+              className={`${graphTableButtonStyle} ${popularityDisplay == "table"
+                ? "bg-bcgw-gray-light"
+                : "bg-[#f5f5f5]"
+                }`}
               onClick={() => setPopularityDisplay("table")}
             >
               Table
