574385e1d65e9f926d96442649871586754d1469
Ramy Kaddouri <myramkadd@gmail.com>
2025-12-24 14:03:55 -0500

    modal responsiveness fixes

--- react-app/src/components/Modal.tsx
+++ react-app/src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 interface ModalPropsType {
   open: boolean;
@@ -21,20 +21,25 @@ const Modal = ({
 }: ModalPropsType): React.ReactElement => {
   const heightString = height ? height + "px" : "auto";
 
+  const handleClose = useCallback(() => {
+    if (!disabled && closable) onClose();
+  }, [closable, disabled, onClose]);
+
   return (
     <>
       {open && (
         <>
           <div
             className="fixed w-screen h-screen z-50 bg-[rgba(0,0,0,0.4)] -translate-x-2/4 -translate-y-2/4 left-2/4 top-2/4"
-            onClick={() => {
-              if (!disabled && closable) onClose();
-            }}
+            onClick={handleClose}
           />
-          <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-50 flex justify-center items-center px-3 sm:px-0 w-full">
+          <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-50 flex justify-center items-center px-3 sm:px-0 w-full"
+            onClick={handleClose}
+          >
             <div
-              className="bg-white rounded-lg shadow-xl overflow-hidden min-w-[300px]"
-              style={{ width: `${width}px`, minHeight: heightString }}
+              className="bg-white rounded-lg shadow-xl overflow-hidden min-w-[300px] w-full"
+              style={{ maxWidth: `${width}px`, minHeight: heightString }}
+              onClick={e => e.stopPropagation()}
             >
               {children}
             </div>
