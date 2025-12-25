63f768bbd2dc0f950d93756c1b76c811af57e59c
Ramy Kaddouri <myramkadd@gmail.com>
2025-12-25 13:48:59 -0500

    implement acuity webhook verification middleware

---
+++ cloud-functions/functions/src/middleware/acuityWebhook.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import { config } from "../config";
+import crypto from "crypto"
+
+export function verfiyAcuityWebhook(req: Request, res: Response, next: NextFunction) {
+  const key = config.acuityAPIKey.value()
+  const body = req.body;
+
+  const hmac = crypto.createHmac("sha256", key);
+  hmac.update(body);
+  const expectedSignature = hmac.digest("base64");
+  const signature = req.headers["x-acuity-signature"];
+
+  if (expectedSignature === signature) {
+    return next();
+  } else {
+    return res.status(403).send("Webhook signature could not be verified")
+  }
+}
