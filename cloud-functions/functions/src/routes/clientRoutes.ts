0b466d9b3d2ba0e33b7b2ac6d54819cbdf3e6a5e
Ramy Kaddouri <myramkadd@gmail.com>
2025-12-25 13:05:30 -0500

    implement acuity sync route

--- cloud-functions/functions/src/routes/clientRoutes.ts
+++ cloud-functions/functions/src/routes/clientRoutes.ts
@@ -1,10 +1,21 @@
 import { Request, Response, Router } from "express";
 import { isAuthenticated } from "../middleware/authMiddleware";
+import { syncAcuityClients } from "../services/sync/acuitySync";
+import { DateTime } from "luxon";
 
 const router = Router();
 
 router.post("/sync/acuity", [isAuthenticated], async (req: Request, res: Response) => {
+  const startDate = req.params.startDate ?? DateTime.now().minus({ months: 1 }).toISODate()
+  const endDate = req.params.endDate ?? DateTime.now()
 
+  const result = await syncAcuityClients(startDate, endDate);
+
+  if (result.status === "OK") {
+    return res.json(result);
+  } else {
+    return res.status(400).json(result)
+  }
 })
 
 router.post("/hooks/acuity/client")
