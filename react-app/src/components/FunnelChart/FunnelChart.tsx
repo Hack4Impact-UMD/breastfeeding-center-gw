83a6755efac73c769cf06f0b6a17455d3bb7741b
Ramy Kaddouri <myramkadd@gmail.com>
2025-12-31 17:17:10 -0500

    use next pt instead

--- react-app/src/components/FunnelChart/FunnelChart.tsx
+++ react-app/src/components/FunnelChart/FunnelChart.tsx
@@ -11,15 +11,16 @@ type FunnelChartProps = {
   data: FunnelChartDataPoint[]
 }
 
-function FunnelChartBlock({ offset, width, prev, pt, max }: { offset: number, width: number, max: number, prev: FunnelChartDataPoint, pt: FunnelChartDataPoint }) {
-  const prevVal = 25 * prev.value / max;
-  const currVal = 25 * pt.value / max;
-  const padding = 10;
+function FunnelChartBlock({ offset, width, next, pt, max }: { offset: number, width: number, max: number, next: FunnelChartDataPoint, pt: FunnelChartDataPoint }) {
+  const nextVal = 30 * next.value / max;
+  const currVal = 30 * pt.value / max;
+  const padding = 7;
+  const spacing = 0.2;
   const pts = [
-    [offset + width, currVal + padding],
-    [offset, prevVal + padding],
-    [offset, -prevVal - padding],
-    [offset + width, -currVal - padding],
+    [offset + width - spacing, nextVal + padding],
+    [offset + spacing, currVal + padding],
+    [offset + spacing, -currVal - padding],
+    [offset + width - spacing, -nextVal - padding],
   ];
 
   const center = [offset + width / 2, 0]
@@ -50,8 +51,8 @@ export default function FunnelChart({ data }: FunnelChartProps) {
   return <svg viewBox="0 -15 100 30" xmlns="http://www.w3.org/2000/svg" className="w-full h-full">
     <text x="4" y="0" textAnchor="middle" transform={"rotate(270, 4, 0)"} fontSize={4.5} fontWeight={"bold"} className="fill-black">Number of Clients</text>
     {data.map((d, index) => {
-      const prev = data[index - 1] ?? data[0];
-      return <FunnelChartBlock max={max} width={(100 - 6) / data.length} offset={6 + index * ((100 - 6) / data.length)} prev={prev} pt={d} />
+      const next = data[index + 1] ?? data[data.length - 1];
+      return <FunnelChartBlock max={max} width={(100 - 6) / data.length} offset={6 + index * ((100 - 6) / data.length)} next={next} pt={d} />
     })}
   </svg>
 }
