caaf3b8d5c2d98f412836d7b9c01fe4a997cc141
Ramy Kaddouri <myramkadd@gmail.com>
2026-01-16 22:57:43 -0500

    add home page carousel + carousel slide components

---
+++ react-app/src/components/HomeStats/HomeStatsCarousel.tsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import { Carousel, CarouselApi, CarouselContent } from "../ui/carousel";
+import { Button } from "../ui/button";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import HomeCarouselSlide from "./HomeCarouselSlide";
+
+export default function HomeStatsCarousel() {
+  const [carouselApi, setCarouselApi] = useState<CarouselApi | null>(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [totalItems, setTotalItems] = useState(0);
+
+  useEffect(() => {
+    if (!carouselApi) return;
+
+    const updateCarouselState = () => {
+      setCurrentIndex(carouselApi.selectedScrollSnap());
+      setTotalItems(carouselApi.scrollSnapList().length);
+    };
+
+    updateCarouselState();
+
+    carouselApi.on("select", updateCarouselState);
+
+    return () => {
+      carouselApi.off("select", updateCarouselState); // Clean up on unmount
+    };
+  }, [carouselApi]);
+
+  const scrollToIndex = (index: number) => {
+    carouselApi?.scrollTo(index);
+  };
+
+  return (
+    <div className="relative h-128 max-h-[500px] w-full max-w-7xl">
+      <Carousel
+        setApi={setCarouselApi}
+        opts={{ loop: true }}
+        className="w-full max-w-7xl h-96 max-h-[500px] z-10"
+      >
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <HomeCarouselSlide key={index} className="relative items-center justify-center h-128 max-h-[500px] p-6">
+              <svg className="absolute inset-0" viewBox="0 0 1355 529" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="233" cy="146" r="233" fill="#0F4374" fillOpacity="0.5" />
+                <circle cx="1292" cy="379" r="150" fill="#0F4374" fillOpacity="0.5" />
+              </svg>
+              <div className="flex items-center justify-center w-full h-full">
+                <span className="text-4xl font-semibold">{index + 1}</span>
+              </div>
+            </HomeCarouselSlide>
+          ))}
+        </CarouselContent>
+      </Carousel>
+
+      <div className="absolute inset-0 z-20 flex items-center justify-between pointer-events-none text-white">
+        <Button
+          onClick={() => scrollToIndex(currentIndex - 1)}
+          className="pointer-events-auto rounded-full p-1 bg-transparent shadow-none hover:bg-transparent"
+        >
+          <ChevronLeft className="size-20" strokeWidth={1.5} />
+        </Button>
+        <Button
+          onClick={() => scrollToIndex(currentIndex + 1)}
+          className="pointer-events-auto rounded-full p-1 bg-transparent shadow-none hover:bg-transparent"
+        >
+          <ChevronRight className="size-20" strokeWidth={1.5} />
+        </Button>
+      </div>
+
+      <div className="absolute bottom-4 left-0 right-0 flex justify-center space-x-2 z-20">
+        {Array.from({ length: totalItems }).map((_, index) => (
+          <Button
+            key={index}
+            onClick={() => scrollToIndex(index)}
+            className={`w-3 h-3 p-0 rounded-full ${currentIndex === index ? "bg-bcgw-yellow-dark" : "bg-gray-600"
+              }`}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
--- react-app/src/pages/HomePage/HomePage.tsx
+++ react-app/src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,4 @@
+import HomeStatsCarousel from "@/components/HomeStats/HomeStatsCarousel";
 import useAcuityAttendance from "@/hooks/queries/useAcuityAttendance";
 import { useJaneConsultationsCount } from "@/hooks/queries/useJaneConsultationsCount";
 import useMostAttendedAcuityClasses from "@/hooks/queries/useMostAttendedAcuityClasses";
@@ -31,45 +32,51 @@ export default function HomePage() {
   } = useAcuityAttendance(startDate, endDate);
 
   return (
-    <div className="p-4">
-      <p>Home</p>
-      {clientCountPending ? (
-        <p>Loading...</p>
-      ) : clientCountError ? (
-        <p>Failed to fetch new clients</p>
-      ) : (
-        <p>New Jane clients in last month: {newClients}</p>
-      )}
+    <div className="px-4 md:px-8 py-10 flex flex-col items-center gap-4">
+      <div className="w-full max-w-7xl">
+        <h1 className="font-bold">Home Dashboard</h1>
+      </div>
+      <HomeStatsCarousel />
+      <div className="w-full max-w-7xl">
+        <p>Home</p>
+        {clientCountPending ? (
+          <p>Loading...</p>
+        ) : clientCountError ? (
+          <p>Failed to fetch new clients</p>
+        ) : (
+          <p>New Jane clients in last month: {newClients}</p>
+        )}
 
-      {apptsCountPending ? (
-        <p>Loading...</p>
-      ) : apptsCountError ? (
-        <p>Failed to fetch jane consultations</p>
-      ) : (
-        <p>Jane consultations in last month: {apptsCount}</p>
-      )}
+        {apptsCountPending ? (
+          <p>Loading...</p>
+        ) : apptsCountError ? (
+          <p>Failed to fetch jane consultations</p>
+        ) : (
+          <p>Jane consultations in last month: {apptsCount}</p>
+        )}
 
-      {classesPending ? (
-        <p>Loading...</p>
-      ) : classesError ? (
-        <p>Failed to fetch acuity classes</p>
-      ) : (
-        <p>
-          Most Popular Classes:{" "}
-          {mostAttendedClasses
-            ?.slice(0, 3)
-            .map((c) => `${c.class} (${c.attendance})`)
-            .join(", ")}
-        </p>
-      )}
+        {classesPending ? (
+          <p>Loading...</p>
+        ) : classesError ? (
+          <p>Failed to fetch acuity classes</p>
+        ) : (
+          <p>
+            Most Popular Classes:{" "}
+            {mostAttendedClasses
+              ?.slice(0, 3)
+              .map((c) => `${c.class} (${c.attendance})`)
+              .join(", ")}
+          </p>
+        )}
 
-      {attendancePending ? (
-        <p>Loading...</p>
-      ) : attendanceError ? (
-        <p>Failed to fetch acuity attendance</p>
-      ) : (
-        <p>Acuity attendance in last month: {acuityAttendance}</p>
-      )}
+        {attendancePending ? (
+          <p>Loading...</p>
+        ) : attendanceError ? (
+          <p>Failed to fetch acuity attendance</p>
+        ) : (
+          <p>Acuity attendance in last month: {acuityAttendance}</p>
+        )}
+      </div>
     </div>
   );
 }
