a6df8f20cd19b79e137526cba05984d49975be5d
Ramy Kaddouri <myramkadd@gmail.com>
2026-02-07 23:48:00 -0500

    display new rentals by week on booqable page

--- react-app/src/pages/BooqableDashboardPage.tsx
+++ react-app/src/pages/BooqableDashboardPage.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { BarChart, BarSeries, Bar, BarProps } from "reaviz";
+import { useMemo, useState } from "react";
+import { BarChart, BarSeries, Bar, BarProps, LineChart } from "reaviz";
 import {
   DateRangePicker,
   defaultPresets,
@@ -15,6 +15,10 @@ import ExportTrigger from "@/components/export/ExportTrigger";
 import ExportContent from "@/components/export/ExportContent";
 import ExportOnly from "@/components/export/ExportOnly";
 import { exportCsv } from "@/lib/tableExportUtils";
+import { useBooqableRentalsInRange } from "@/hooks/queries/useBooqableRentalsInRange";
+import { BooqableRental } from "@/services/booqableService";
+import { DateTime } from "luxon";
+import Loading from "@/components/Loading";
 
 // record of colors to use for each rental item
 const colors: Record<string, string> = {
@@ -39,18 +43,6 @@ function CustomBar(props: Partial<BarProps>) {
   );
 }
 
-// getting the date from user input
-// function getWeekRange(date: Date) {
-//   const selected = new Date(date);
-//   const day = selected.getDay(); // 0 (Sun) to 6 (Sat)
-//   const diffToMonday = (day === 0 ? -6 : 1) - day;
-//   const monday = new Date(selected);
-//   monday.setDate(selected.getDate() + diffToMonday);
-//   const sunday = new Date(monday);
-//   sunday.setDate(monday.getDate() + 6);
-//   return { monday, sunday };
-// }
-
 // format the date to put in title
 function formatDate(date: Date) {
   const m = date.getMonth() + 1;
@@ -59,12 +51,60 @@ function formatDate(date: Date) {
   return `${m}/${d}/${y}`;
 }
 
+function useRentalsByWeek(rentals: BooqableRental[], startDate?: string, endDate?: string) {
+  return useMemo(() => {
+    if (!startDate || !endDate) return new Map();
+    const rentalsByWeek = new Map<string, BooqableRental[]>()
+
+    for (const rental of rentals) {
+      const week = DateTime.fromISO(rental.firstPayDate).startOf("week").toISO();
+      if (!week) continue;
+
+      if (rentalsByWeek.has(week)) {
+        rentalsByWeek.get(week)!.push(rental);
+      } else {
+        rentalsByWeek.set(week, [rental]);
+      }
+    }
+
+    let start = DateTime.fromISO(startDate).startOf("week");
+    const end = DateTime.fromISO(endDate).endOf("week")
+
+    while (start < end) {
+      const startIso = start.toISO();
+      if (startIso) {
+        if (!rentalsByWeek.has(startIso)) {
+          rentalsByWeek.set(startIso, []);
+        }
+      }
+      start = start.plus({ week: 1 });
+    }
+
+    return rentalsByWeek;
+  }, [rentals, startDate, endDate])
+}
+
 export default function BooqableDashboardPage() {
   // use state for toggles/buttons/changing of information
   const [rentalDisplay, setRentalDisplay] = useState("graph");
   const [dateRange, setDateRange] = useState<DateRange | undefined>(
     defaultDateRange,
   );
+  const { data: rentals, isPending, error } = useBooqableRentalsInRange(dateRange?.from?.toISOString(), dateRange?.to?.toISOString())
+
+  const rentalsByWeek = useRentalsByWeek(rentals ?? [], dateRange?.from?.toISOString(), dateRange?.to?.toISOString());
+  const chartData = useMemo(() => {
+    const points = []
+    for (const [week, rentals] of rentalsByWeek.entries()) {
+      points.push({
+        data: rentals.length,
+        key: new Date(week)
+      })
+    }
+    return points.sort((a, b) => a.key.getTime() - b.key.getTime())
+  }, [rentalsByWeek]);
+
+
   const dateRangeLabel =
     dateRange?.from && dateRange?.to
       ? `${formatDate(dateRange.from)} - ${formatDate(dateRange.to)}`
@@ -74,51 +114,36 @@ export default function BooqableDashboardPage() {
     "py-1 px-4 text-center shadow-sm bg-[#f5f5f5] hover:shadow-md text-black cursor-pointer";
   const centerItemsInDiv = "flex justify-between items-center";
 
-  // data
-  const dataMonths = [
-    { key: "Medela Symphony", data: 14 },
-    { key: "Ameda Platinum", data: 12 },
-    { key: "Spectra S3", data: 9 },
-    { key: "Medela BabyWeigh Scale", data: 9 },
-    { key: "Medela BabyCheck Scale", data: 13 },
-  ];
-
-  // sample data
-  type Rental = {
-    item: string;
-    durationMonths: number;
-  };
+  // // sample data
+  // type RentalTableRow = {
+  //   week: Date;
+  //   rentals: number,
+  //   totalRentalAmount: number,
+  //   averageRentalAmount: number
+  // };
 
   // data for table
-  const columns: ColumnDef<Rental>[] = [
-    {
-      accessorKey: "item",
-      header: ({ column }) => {
-        return <ColumnSortButton column={column}>Item</ColumnSortButton>;
-      },
-      cell: ({ row }) => (
-        <span className="font-bold">{row.getValue("item")}</span>
-      ),
-    },
-    {
-      accessorKey: "durationMonths",
-      header: ({ column }) => {
-        return (
-          <ColumnSortButton column={column}>
-            Average Rental Duration (Months)
-          </ColumnSortButton>
-        );
-      },
-    },
-  ];
-
-  const mockData: Rental[] = [
-    { item: "Medela Symphony", durationMonths: 2 },
-    { item: "Ameda Platinum", durationMonths: 3 },
-    { item: "Spectra S3", durationMonths: 4 },
-    { item: "Medela Babyweigh Scale", durationMonths: 3 },
-    { item: "Medela Babychecker Scale", durationMonths: 3 },
-  ];
+  // const columns: ColumnDef<Rental>[] = [
+  //   {
+  //     accessorKey: "item",
+  //     header: ({ column }) => {
+  //       return <ColumnSortButton column={column}>Week</ColumnSortButton>;
+  //     },
+  //     cell: ({ row }) => (
+  //       <span className="font-bold">{row.getValue("item")}</span>
+  //     ),
+  //   },
+  //   {
+  //     accessorKey: "durationMonths",
+  //     header: ({ column }) => {
+  //       return (
+  //         <ColumnSortButton column={column}>
+  //           Average Rental Duration (Months)
+  //         </ColumnSortButton>
+  //       );
+  //     },
+  //   },
+  // ];
 
   return (
     <>
@@ -141,82 +166,91 @@ export default function BooqableDashboardPage() {
             />
           </div>
         </div>
-
-        {/* Graph/Table & Export Selection */}
-        <Export title={`AverageRentalDuration${dateRangeLabel}`}>
-          <div className={`${centerItemsInDiv} pt-4`}>
-            <div className="flex flex-row">
-              <button
-                className={`${graphTableButtonStyle} ${rentalDisplay == "graph"
-                  ? "bg-bcgw-gray-light"
-                  : "bg-[#f5f5f5]"
-                  }`}
-                onClick={() => setRentalDisplay("graph")}
-              >
-                Graph
-              </button>
-              <button
-                className={`${graphTableButtonStyle} ${rentalDisplay == "table"
-                  ? "bg-bcgw-gray-light"
-                  : "bg-[#f5f5f5]"
-                  }`}
-                onClick={() => setRentalDisplay("table")}
-              >
-                Table
-              </button>
-            </div>
-            {rentalDisplay === "table" ? (
-              <Button
-                variant={"outlineGray"}
-                className={
-                  "text-md rounded-full border-2 py-4 px-6 shadow-md hover:bg-bcgw-gray-light"
-                }
-                onClick={() =>
-                  exportCsv(
-                    mockData,
-                    `booqable_avg_rental_duration${dateRange?.from?.toISOString()}_${dateRange?.to?.toISOString()}`,
-                  )
-                }
-              >
-                Export
-              </Button>
-            ) : (
-              <ExportTrigger disabled={rentalDisplay !== "graph"} asChild>
-                <Button
-                  variant={"outlineGray"}
-                  className={
-                    "text-md rounded-full border-2 py-4 px-6 shadow-md hover:bg-bcgw-gray-light"
-                  }
-                >
-                  Export
-                </Button>
-              </ExportTrigger>
-            )}
-          </div>
-
-          <span className="self-start font-semibold text-2xl">
-            Average Rental Duration By Item, {dateRangeLabel}
-          </span>
-          {rentalDisplay === "graph" ? (
-            <div className="flex flex-col items-center justify-start bg-white min-h-[400px] border-2 border-black p-5 mt-5 rounded-2xl">
-              <ExportContent className="w-full">
-                <ExportOnly className="mb-5">
-                  <h1 className="text-xl font-bold text-black">
-                    Average Rental Duration by Item
-                  </h1>
-                  <p className="text-base text-black">{dateRangeLabel}</p>
-                </ExportOnly>
-                <BarChart
-                  height={500}
-                  data={dataMonths}
-                  series={<BarSeries layout="vertical" bar={<CustomBar />} />}
-                />
-              </ExportContent>
-            </div>
+        {
+          isPending ? (
+            <Loading />
+          ) : error ? (
+            <p className="text-red-600">Failed to fetch Booqable data: {error.message}</p>
           ) : (
-            <DataTable columns={columns} data={mockData} tableType="default" />
-          )}
-        </Export>
+            <>
+              {/* Graph/Table & Export Selection */}
+              <Export title={`NewRentalsByWeek${dateRangeLabel}`}>
+                <div className={`${centerItemsInDiv} pt-4`}>
+                  <div className="flex flex-row">
+                    <button
+                      className={`${graphTableButtonStyle} ${rentalDisplay == "graph"
+                        ? "bg-bcgw-gray-light"
+                        : "bg-[#f5f5f5]"
+                        }`}
+                      onClick={() => setRentalDisplay("graph")}
+                    >
+                      Graph
+                    </button>
+                    <button
+                      className={`${graphTableButtonStyle} ${rentalDisplay == "table"
+                        ? "bg-bcgw-gray-light"
+                        : "bg-[#f5f5f5]"
+                        }`}
+                      onClick={() => setRentalDisplay("table")}
+                    >
+                      Table
+                    </button>
+                  </div>
+                  {rentalDisplay === "table" ? (
+                    <Button
+                      variant={"outlineGray"}
+                      className={
+                        "text-md rounded-full border-2 py-4 px-6 shadow-md hover:bg-bcgw-gray-light"
+                      }
+                      onClick={() =>
+                        exportCsv(
+                          [],
+                          `booqable_avg_rental_duration${dateRange?.from?.toISOString()}_${dateRange?.to?.toISOString()}`,
+                        )
+                      }
+                    >
+                      Export
+                    </Button>
+                  ) : (
+                    <ExportTrigger disabled={rentalDisplay !== "graph"} asChild>
+                      <Button
+                        variant={"outlineGray"}
+                        className={
+                          "text-md rounded-full border-2 py-4 px-6 shadow-md hover:bg-bcgw-gray-light"
+                        }
+                      >
+                        Export
+                      </Button>
+                    </ExportTrigger>
+                  )}
+                </div>
+
+                <span className="self-start font-semibold text-2xl">
+                  New Rentals By Week, {dateRangeLabel}
+                </span>
+                {rentalDisplay === "graph" ? (
+                  <div className="flex flex-col items-center justify-start bg-white min-h-[400px] border-2 border-black p-5 mt-5 rounded-2xl">
+                    <ExportContent className="w-full">
+                      <ExportOnly className="mb-5">
+                        <h1 className="text-xl font-bold text-black">
+                          New Rentals By Week
+                        </h1>
+                        <p className="text-base text-black">{dateRangeLabel}</p>
+                      </ExportOnly>
+                      <LineChart
+                        height={500}
+                        data={chartData}
+                      />
+                    </ExportContent>
+                  </div>
+                ) : (
+                  <DataTable columns={[]} data={[]} tableType="default" />
+                )}
+              </Export>
+            </>
+          )
+        }
+
       </div>
     </>
   );
