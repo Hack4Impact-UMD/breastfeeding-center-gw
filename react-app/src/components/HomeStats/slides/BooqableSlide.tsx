75604ffdcfe9837375d1acd4a33f857810177f97
Ramy Kaddouri <myramkadd@gmail.com>
2026-02-10 10:02:23 -0500

    hook up booqable slide

--- react-app/src/components/HomeStats/slides/BooqableSlide.tsx
+++ react-app/src/components/HomeStats/slides/BooqableSlide.tsx
@@ -1,17 +1,21 @@
 import { DateTime } from "luxon";
 import HomeCarouselSlide from "../HomeCarouselSlide";
 import booqableIcon from "@/assets/icons/booqableIcon.png";
-import { Package } from "lucide-react";
-import { Award } from "lucide-react";
+import { Package, UserIcon } from "lucide-react";
+import { useBooqableRentalsInRange } from "@/hooks/queries/useBooqableRentalsInRange";
+import Loading from "@/components/Loading";
+import { useMemo } from "react";
 
 export default function BooqableSlide() {
   const startDate = DateTime.now().minus({ days: 30 }).startOf("day");
   const endDate = DateTime.now().endOf("day");
 
-  // TODO: Connect to Booqable API to fetch real data
-  const numItemsRented: number = 42;
-  const mostRentedItem: string = "Hospital Grade Pump";
-  const numMostRentedItem: number = 23;
+  const { data: rentals, isPending, error } = useBooqableRentalsInRange(startDate.toISO(), endDate.toISO());
+
+  const uniqueClients: number = useMemo(() => {
+    const clients = new Set<string>(rentals?.map(r => r.customerId));
+    return clients.size
+  }, [rentals]);
 
   return (
     <HomeCarouselSlide className="relative h-128 max-h-[500px] p-6 px-12 lg:px-18">
@@ -25,65 +29,58 @@ export default function BooqableSlide() {
         <circle cx="1292" cy="379" r="150" fill="#0F4374" fillOpacity="0.5" />
       </svg>
       <div className="flex flex-col gap-4 lg:gap-10 items-center justify-center w-full h-full max">
-        {/* {numItemsRentedPending || mostRentedItemPending ? (
+        {isPending ? (
           <Loading />
-        ) : numItemsRentedError ? (
-          <p className="z-10">Failed to fetch Booqable number items rented: {numItemsRentedError.message}</p>
-        ) : mostRentedItemError ? (
-          <p className="z-10">Failed to fetch Booqable most rented item: {mostRentedItemError.message}</p>
-        ) : ( */}
-        <>
-          <div className="flex flex-row gap-4 lg:gap-8 z-10 items-center justify-center">
-            <div className="bg-bcgw-yellow-dark rounded-full p-1 size-10 min-w-10 min-h-10 lg:size-20 lg:min-w-20 lg:min-h-20 flex items-center justify-center">
-              <img
-                src={booqableIcon}
-                alt="Booqable icon"
-                className="size-3/4 object-contain"
-              />
+        ) : error ? (
+          <p className="z-10">Failed to fetch Booqable rentals: {error.message}</p>
+        ) : (
+          <>
+            <div className="flex flex-row gap-4 lg:gap-8 z-10 items-center justify-center">
+              <div className="bg-bcgw-yellow-dark rounded-full p-1 size-10 min-w-10 min-h-10 lg:size-20 lg:min-w-20 lg:min-h-20 flex items-center justify-center">
+                <img
+                  src={booqableIcon}
+                  alt="Booqable icon"
+                  className="size-3/4 object-contain"
+                />
+              </div>
+              <div>
+                <h1 className="font-semibold text-lg lg:text-4xl">
+                  BOOQABLE STATS: LAST 30 DAYS
+                </h1>
+                <p className="text-xs md:text-sm">
+                  {startDate.toLocaleString({
+                    month: "long",
+                    day: "numeric"
+                  })} - {endDate.toLocaleString({
+                    month: "long",
+                    day: "numeric"
+                  })}
+                </p>
+              </div>
             </div>
-            <div>
-              <h1 className="font-semibold text-lg lg:text-4xl">
-                BOOQABLE STATS: LAST 30 DAYS
-              </h1>
-              <p className="text-xs md:text-sm">
-                {startDate.toLocaleString({
-                  month: "long",
-                  day: "numeric"
-                })} - {endDate.toLocaleString({
-                  month: "long",
-                  day: "numeric"
-                })}
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-col lg:flex-row gap-4 w-full max-w-4xl z-10 justify-center items-center">
-            <div className="max-w-120 max-h-63 w-full h-full bg-white rounded-lg flex flex-col items-center justify-center p-4 lg:py-6 lg:gap-2">
-              <Package className="size-8 lg:size-15 p-2 bg-bcgw-yellow-dark rounded-full text-black" />
-              <h1 className="text-bcgw-blue-light font-semibold text-3xl md:text-6xl">
-                {numItemsRented}
-              </h1>
-              <span className="text-black lg:text-xl text-center">
-                Items Rented
-              </span>
-            </div>
-
-            <div className="max-w-120 max-h-63 w-full h-full bg-white rounded-lg flex flex-col items-center justify-center p-4 lg:py-6 lg:gap-2">
-              <Award className="size-8 lg:size-15 p-2 bg-bcgw-yellow-dark rounded-full text-black" />
-              <h1 className="text-bcgw-blue-light font-semibold text-xl md:text-3xl text-center">
-                {mostRentedItem}
-              </h1>
-              <div className="flex flex-col gap-1">
+            <div className="flex flex-col lg:flex-row gap-4 w-full max-w-4xl z-10 justify-center items-center">
+              <div className="max-w-120 max-h-63 w-full h-full bg-white rounded-lg flex flex-col items-center justify-center p-4 lg:py-6 lg:gap-2">
+                <Package className="size-8 lg:size-15 p-2 bg-bcgw-yellow-dark rounded-full text-black" />
+                <h1 className="text-bcgw-blue-light font-semibold text-3xl md:text-6xl">
+                  {rentals.length}
+                </h1>
                 <span className="text-black lg:text-xl text-center">
-                  Most Rented Item
+                  New Rentals Started
                 </span>
-                <span className="text-[#434343] lg:text-base text-center">
-                  {numMostRentedItem} rentals
+              </div>
+
+              <div className="max-w-120 max-h-63 w-full h-full bg-white rounded-lg flex flex-col items-center justify-center p-4 lg:py-6 lg:gap-2">
+                <UserIcon className="size-8 lg:size-15 p-2 bg-bcgw-yellow-dark rounded-full text-black" />
+                <h1 className="text-bcgw-blue-light font-semibold text-3xl md:text-6xl">
+                  {uniqueClients}
+                </h1>
+                <span className="text-black lg:text-xl text-center">
+                  Unique Clients
                 </span>
               </div>
             </div>
-          </div>
-        </>
-        {/* )} */}
+          </>
+        )}
       </div>
     </HomeCarouselSlide>
   );
